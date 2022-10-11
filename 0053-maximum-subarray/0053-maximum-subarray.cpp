ce7a9cfc1d15dde4e2c2752f586a503ec9c15ba7
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-11 17:42:00 +0200

    Time: 226 ms (42.63%), Space: 67.8 MB (53.07%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    int maxSubArray(vector<int>& nums) 
+    {
+        int maxSum = nums[0], curSum = nums[0];
+        for(int i = 1 ; i < (int)nums.size() ; i++)
+        {
+            if(nums[i] >= curSum && curSum < 0)
+                curSum = 0;
+            
+            curSum += nums[i];
+            
+            if(curSum > maxSum)
+                maxSum = curSum;
+        }
+        return maxSum;
+    }
+};
