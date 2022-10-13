ac7296a522a3e88fe73d1d85b05c5d67e1b102f2
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-13 11:06:53 +0200

    Time: 7 ms (81.20%), Space: 10.3 MB (54.92%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -7,12 +7,16 @@ public:
      *
      * Complexity:
      * Time Complexity : O(n+m)
-     * Space Complexity : O(n)
+     * Space Complexity : O(min(n,m))
      */
-
     
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
     {
+        // What if nums1's size is small compared to nums2's size? Which algorithm is better?
+        // Swap nums1 and nums2 to make nums1 always smaller for minimum space complexity
+        if((int)nums1.size() > (int)nums2.size())
+            return intersect(nums2, nums1);
+            
         unordered_map<int, int> ump;
         vector<int> result;
         for(int i = 0 ; i < (int)nums1.size() ; i++)
