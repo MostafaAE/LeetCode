cb2a19c0d51e608a3a1e814913b391edd676df93
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-15 16:23:11 +0200

    Time: 40 ms (26.18%), Space: 8.7 MB (72.68%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * store the frequency of each letter in each string
+    * compare the letter frequencies of the two strings
+    * if frequency of all letters in ransomNote <= frequency of all letter in magazine
+    * then this is a valid ransomNote
+    * 
+    * Complexity:
+    * Time Complexity : O(m+r)
+    * Space Complexity : O(1)
+    */
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        int m = (int)magazine.size();
+        int r = (int)ransomNote.size();
+        if(m < r)
+            return false;
+        
+        int mFreq[26]{}, rFreq[26]{};
+        
+        // O(m)
+        for(int i = 0 ; i < m ; i++)
+            mFreq[magazine[i]-'a']++;
+        
+        // O(r)
+        for(int i = 0 ; i < r ; i++)
+            rFreq[ransomNote[i]-'a']++;
+        
+        //O(1)
+        for(int i = 0 ; i < 26 ; i++)
+            if(rFreq[i] > mFreq[i])
+                return false;
+
+        return true;
+        
+    }
+};
