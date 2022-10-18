b2d742f6286d02733ab723cd30c22f37e5341a44
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-18 16:44:22 +0200

    Time: 30 ms (41.43%), Space: 6.1 MB (9.16%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,23 +2,39 @@ class Solution {
 public:
     /* 
     * Approach:
-    * Convert the integer to a string and check if it is a palindrome
+    * Get the number of digits in the input number, keep extracting and comparing
+    * then dropping the leftmost and rightmost digits from the input until
+    * the number of digits become <= 1
     * 
     * Complexity:
-    * Time Complexity : O(L) where L is the number of digits in the input
-    * Space Complexity : O(L)
+    * Time Complexity : O(n) where n is the number of digits in the input
+    * Space Complexity : O(1)
     */
     bool isPalindrome(int x) 
     {
         if(x < 0)
             return false;
+        if (x == 0)
+            return true;
         
-        string s = to_string(x);
-        int start{}, end = (int)s.size()-1;
+        int digitsNum = log10(x)+1;
         
-        while(start < end)
-            if(s[start++] != s[end--])
+        while(digitsNum > 1)
+        {
+            // extract leftmost digit
+            int leftDigitTens = pow(10, digitsNum-1);
+            int leftDigit = x / leftDigitTens;
+            
+            // extract rightmost digit
+            int rightDigit = x % 10;
+            
+            if(leftDigit != rightDigit)
                 return false;
+            
+            // drop the leftmost and rightmost digits from the number
+            x = (x - leftDigit*leftDigitTens)/10;
+            digitsNum-=2;
+        }
         
         return true;
         
