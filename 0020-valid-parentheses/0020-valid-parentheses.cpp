00eace2ff045cb75926fd907a65636a0b703f98f
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-19 12:23:35 +0200

    Time: 7 ms (12.67%), Space: 6.4 MB (10.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * use a stack to check if every open parenthese has a matching closing parenthese
+    * 
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(n)
+    */
+    bool isValid(string s) 
+    {
+        map<char,char> mp {{')', '('}, {']', '['}, {'}', '{'}};
+        stack<char> st;
+        
+        for(char c : s)
+        {
+            if(c == '(' || c == '[' || c == '{')
+                st.push(c);
+            else if(st.empty() || st.top() != mp[c])
+                return false;
+            else
+                st.pop();
+        }
+        return st.empty();
+    }
+};
