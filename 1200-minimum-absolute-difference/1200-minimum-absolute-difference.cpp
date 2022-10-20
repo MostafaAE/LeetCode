3334635888fd9d7e49e1e15106427bde24c4b23a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-20 21:54:38 +0200

    Time: 134 ms (57.98%), Space: 32.2 MB (49.70%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -9,32 +9,25 @@ public:
     * Time Complexity : O(nlogn)
     * Space Complexity : O(1) >> neglecting the output space
     */
-    vector<vector<int>> minimumAbsDifference(vector<int>& arr) 
+    vector<vector<int>> minimumAbsDifference(vector<int> &arr)
     {
         vector<vector<int>> result;
         int minAbsDif{INT_MAX};
-        
+
         // O(nlogn)
         sort(arr.begin(), arr.end());
-        
+
         // find the minimum absolute difference
         // O(n)
-        for(int i = 1 ; i < (int)arr.size() ; i++)
-            if(arr[i] - arr[i-1] < minAbsDif)
-                minAbsDif = arr[i] - arr[i-1];
-        
+        for (int i = 1; i < (int)arr.size(); i++)
+            minAbsDif = min(arr[i] - arr[i - 1], minAbsDif);
+
         // push each pair with the minimum abs diff in the result
         // O(n)
-        for(int i = 1 ; i < (int)arr.size() ; i++)
-        {
-            vector<int> p(2);
-            if(arr[i] - arr[i-1] == minAbsDif)
-            {
-                p[0] = arr[i-1], p[1] = arr[i];
-                result.push_back(p);
-            }
-        }
-        
+        for (int i = 1; i < (int)arr.size(); i++)
+            if (arr[i] - arr[i - 1] == minAbsDif)
+                result.push_back({arr[i - 1], arr[i]});
+
         return result;
     }
 };
