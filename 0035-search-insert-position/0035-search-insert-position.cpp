4e046bf5b17a5297a0c8abcc4fa21ec35d891b71
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-20 13:51:38 +0200

    Time: 14 ms (20.65%), Space: 9.6 MB (94.62%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    /* 
+    * Approach:
+    * Binary search
+    * 
+    * Complexity:
+    * Time Complexity : O(logn)
+    * Space Complexity : O(1)
+    */
     int searchInsert(vector<int>& nums, int target) 
     {
         int start{}, end{(int)nums.size()-1};
         
-        while(start < end)
+        while(start <= end)
         {
             int mid = (start+end)/2;
+            
+            if(target == nums[mid])
+                return mid;
             
             if(target < nums[mid])
                 end = mid-1;
             
-            else if(target > nums[mid])
-                start = mid+1;
-            
             else
-                return mid;
+                start = mid+1;
         }
-        return nums[start] < target ? start+1 : start;
+        return start;
         
     }
 };
