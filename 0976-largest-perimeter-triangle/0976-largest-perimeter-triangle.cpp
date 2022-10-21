5cb248ca8c71bb70c6df0f5890a736cc84cb47bd
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-21 18:11:23 +0200

    Time: 98 ms (21.17%), Space: 22 MB (22.14%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -12,12 +12,14 @@ public:
     */
     int largestPerimeter(vector<int>& nums) 
     {
-        sort(nums.begin(), nums.end(), greater<int>());
+        sort(nums.begin(), nums.end(), greater<int>()); // sort in descending order
         
         for(int i = 0 ; i < (int)nums.size()-2 ; i++)
         {
             int a = nums[i], b = nums[i+1], c = nums[i+2];
-            if(checkValidity(a, b, c))
+            // since the values are sorted, one check is enough
+            // otherwise we will have to use the below function
+            if(a < b + c)
                 return a + b + c;
         }
         
@@ -25,12 +27,13 @@ public:
     }
     
     // function to check if three sides form a triangle or not
-    bool checkValidity(int a, int b, int c)
-    {
-        // check condition
-        if (a + b <= c || a + c <= b || b + c <= a)
-            return false;
-        else
-            return true;
-    }
+    // bool checkValidity(int a, int b, int c)
+    // {
+    //     // check condition
+    //     if (a + b <= c || a + c <= b || b + c <= a)
+    //         return false;
+    //     else
+    //         return true;
+    // }
+    
 };
