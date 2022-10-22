820b44b77d766a52a003bdd9d5022e53bedfea95
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-22 13:00:57 +0200

    Time: 102 ms (7.15%), Space: 27.5 MB (32.64%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
     /* 
+    * Approach:
+    * copy the array and sort it, then find the leftmost and rightmost elements from the unsorted
+    * continious subarray and return the distance between them
+    * 
     * Complexity:
     * Time Complexity : O(nlogn)
     * Space Complexity : O(n)
     */
     int findUnsortedSubarray(vector<int>& nums) 
     {
-        vector<int> sorted(nums);
-        int start {-1}, end{-1};
-        
+        vector<int> sorted (nums);
+        // O(nlogn)
         sort(sorted.begin(), sorted.end());
+        int start{-1}, end{}, size{(int)nums.size()};
         
-        for(int i =  0 ; i < (int)nums.size() ; i++)
+        // find the first element in the unsorted continuous subarray
+        // O(n)
+        for(int i = 0 ; i < size ; i++)
         {
             if(nums[i] != sorted[i])
             {
-                if(start == -1)
-                    start = i;
-                else
-                    end = i;
+                start = i;
+                break;
+            }
+        }
+        
+        // find the last element in the unsorted continuous subarray
+        // O(n)
+        for(int i = size-1 ; i >= 0 ; i--)
+        {
+            if(nums[i] != sorted[i])
+            {
+                end = i;
+                break;
             }
         }
         if(start == -1)
             return 0;
         
-        return end-start+1;
+        return end - start + 1;
+        
     }
 };
