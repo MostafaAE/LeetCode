7f279fe21939911c59108857bb65b883928146f6
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-23 21:18:45 +0200

    Time: 430 ms (31.59%), Space: 66.6 MB (16.23%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * binary search the minimum number of days
+    * 
+    * Complexity:
+    * Time Complexity : O(nlogm) where m is the maximum element in the array
+    * Space Complexity : O(1)
+    */
+    int minDays(vector<int>& bloomDay, int m, int k) 
+    {
+        if((long long)m*k > (int)bloomDay.size())
+            return -1;
+        
+        int start = 1, end = *max_element(bloomDay.begin(), bloomDay.end()), answer {end};
+        
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            
+            if(possibleDay(bloomDay, m, k , mid))
+                end = mid - 1, answer = mid;
+            else
+                start = mid + 1;
+        }
+        
+        return answer;
+    }
+    
+    bool possibleDay(vector<int>& bloomDay, int m, int k, int day)
+    {
+        int cnt{};
+        for(int i = 0 ; i < (int)bloomDay.size() ; i++)
+        {
+            if(bloomDay[i] <= day)
+                cnt++;
+            else
+                cnt = 0;
+            
+            if(cnt == k)
+            {
+                m--;
+                cnt = 0;
+            }
+        }
+        return m <= 0;
+    }
+};
