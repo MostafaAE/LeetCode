5987e181b017a64c3703fdfaa17727457d0b932b
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-25 19:39:50 +0200

    Time: 16 ms (11.45%), Space: 11.4 MB (77.71%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        string str1{}, str2{};
+        for(string s : word1)
+            str1+=s;
+        for(string s : word2)
+            str2+=s;
+        return str1 == str2;
+    }
+};
