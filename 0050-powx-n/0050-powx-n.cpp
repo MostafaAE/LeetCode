125c4871efe8f559639ce9a6f65a621af2ba442b
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-27 00:05:22 +0200

    Time: 3 ms (53.79%), Space: 5.8 MB (81.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -5,18 +5,20 @@ public:
     * Time Complexity : O(logn)
     * Space Complexity : O(logn) => auxiliary space for stack
     */
-    double myPow(double x, long long n) 
+    double myPow(double x, int n) 
     {
         if(n < 0)
-            n = -n, x = 1/x;
+            x = 1/x;
         
-        if(n == 0)
+        long long num = llabs(n);
+        
+        if(num == 0)
             return 1;
         
-        double pow = myPow(x, n/2);
+        double pow = myPow(x, num/2);
 
         // odd
-        if(n&1)
+        if(num&1)
             return x * pow * pow;
         else
             return pow * pow;
