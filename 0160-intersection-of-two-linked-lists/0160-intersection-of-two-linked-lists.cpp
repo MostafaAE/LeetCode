8d94898ae8b1a852a6269036676fbd35cd65154a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-30 23:59:15 +0200

    Time: 102 ms (53.24%), Space: 14.4 MB (98.86%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,26 +11,32 @@ public:
     /* 
     * Complexity:
     * Time Complexity : O(n+m)
-    * Space Complexity : O(n+m)
+    * Space Complexity : O(1)
     */
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
     {
-        unordered_set<ListNode *> s;
+        int countA{}, countB{};
+        ListNode *curA = headA, *curB = headB;
         
-        while(headA || headB)
-        {
-            if(headA)
-            {
-                if(!s.insert(headA).second)
-                    return headA;
+        while(curA)
+            countA++, curA = curA->next;
+        
+        while(curB)
+            countB++, curB = curB->next;
+        
+        if(countA > countB)
+            for(int i = 0 ; i < countA-countB ; i++)
                 headA = headA->next;
-            }
-            if(headB)
-            {
-                if (!s.insert(headB).second)
-                    return headB;
+        else
+            for(int i = 0 ; i < countB-countA ; i++)
                 headB = headB->next;
-            }
+        
+        while(headA && headB)
+        {
+            if(headA == headB)
+                return headA;
+            headA = headA->next;
+            headB = headB->next;
         }
         
         return nullptr;
