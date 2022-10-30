7818902c0266484ebc53b60c8b71c6b2c536d99b
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-10-30 16:48:07 +0200

    Time: 3 ms (86.92%), Space: 5.8 MB (70.97%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     /* 
     * Approach:
-    * binary search based on summation math equation
+    * Binary Search based on summation math equation (monotonic function)
     * 
     * Complexity:
     * Time Complexity : O(logn)
     * Space Complexity : O(1)
     */
-    
-    bool possible(int n, long long rows)
-    {
-        long long summation = rows * (rows+1)/2;
-        return summation <= n;
-    }
     int arrangeCoins(int n) 
     {
         int start = 0, end = n, answer{};
@@ -29,6 +23,12 @@ public:
                 end = mid - 1;
         }
         return answer;
+    }
+    
+    bool possible(int n, long long rows)
+    {
+        long long summation = rows * (rows+1)/2;
+        return summation <= n;
     }
 };
 
