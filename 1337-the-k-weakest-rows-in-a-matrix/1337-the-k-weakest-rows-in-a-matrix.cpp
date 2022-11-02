881bcc567678a67e319d1df4e6815ac4d2899e31
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-02 16:10:39 +0200

    Time: 14 ms (89.95%), Space: 10.6 MB (24.91%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * use binary search to find the first civilian in a each row to get civilians count
+    * and use a priority queue to sort each row's civilians count along thier indecies
+    * 
+    * Complexity:
+    * Time Complexity : O(mlogn)
+    * Space Complexity : O(n)
+    */
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) 
+    {
+        vector<int> result;
+        priority_queue<pair<int,int>> pq;
+        
+        int m = mat.size(), n = mat[0].size();
+        
+        for(int i = 0 ; i < m ; i++)
+        {
+            // O(logn)
+            auto ub = upper_bound(mat[i].rbegin(), mat[i].rend(), 0);
+            int civilians = ub-mat[i].rbegin();
+            
+            // O(logn)
+            pq.push({civilians, -i});
+        }
+        
+        for(int i = 0 ; i < k ; i++)
+            result.push_back(-pq.top().second), pq.pop();
+        
+        return result;
+    }
+};
