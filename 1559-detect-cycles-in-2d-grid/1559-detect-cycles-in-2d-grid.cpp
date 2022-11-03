f984229bb1368aec56bff2a17f5542c859d67e89
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-03 16:31:28 +0200

    Time: 774 ms (21.02%), Space: 52.5 MB (90.72%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -4,6 +4,7 @@ private:
     // delta for: up, down, left, right
     int dr[4]{-1, 1, 0 , 0};
     int dc[4]{0, 0, -1 , 1};
+    bool isCycle{};
 public:
     /* 
     * Approach:
@@ -17,43 +18,43 @@ public:
     {
         int rows{(int)grid.size()}, cols{(int)grid[0].size()};
         vector<vector<bool>> visited(rows, vector<bool>(cols,0));
-        bool cycle{};
+        
         for(int r = 0 ; r < rows ; r++)
         {
             for(int c = 0 ; c < cols ; c++)
             {
                 if(!visited[r][c])
-                    cycle = dfs(grid, r, c, visited, r, c);
+                    dfs(grid, r, c, visited, r, c);
                 
-                if(cycle)
+                if(isCycle)
                     return true;
             }
         }
         return false;
     }
     
-    bool dfs(vector<vector<char>>& grid, int r, int c, vector<vector<bool>>& visited, int prevRow, int prevCol)
+    void dfs(vector<vector<char>>& grid, int r, int c, vector<vector<bool>>& visited, int prevRow, int prevCol)
     {
-        bool isCycle{0};
+        if(!isValid(r, c, grid) || grid[r][c] != grid[prevRow][prevCol] || isCycle)
+            return;
+
+        if(visited[r][c])
+        {
+            isCycle = true;
+            return;
+        }
+        
         visited[r][c] = 1;
         
         for(int d = 0 ; d < 4 ; d++)
         {
             int nr = r + dr[d], nc = c + dc[d];
             
-            if(isValid(nr, nc, grid) && grid[nr][nc] == grid[prevRow][prevCol] && (nr != prevRow || nc != prevCol))
-            {
-                if(visited[nr][nc])
-                {
-                    isCycle = true;
-                    break;
-                }
-                else if(!isCycle)
-                    isCycle |= dfs(grid, nr, nc, visited, r, c);
-            }
-                
+            if(nr == prevRow && nc == prevCol)
+                continue;
+            
+            dfs(grid, nr, nc, visited, r, c);
         }
-        return isCycle;
     }
     
     // return true if this position INSIDE the 2D matrix
