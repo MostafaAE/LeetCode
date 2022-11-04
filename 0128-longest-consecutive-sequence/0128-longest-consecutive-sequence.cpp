81d68f9123683990190baadea72f2d35dc1b20d2
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-04 22:08:01 +0200

    Time: 298 ms (61.20%), Space: 45.8 MB (69.45%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,10 +1,9 @@
-typedef unordered_map<int, int> GRAPH;
 class Solution {
 public:
     /* 
     * Approach:
-    * reduce the problem to a graph and perfrom depth first search to get each connected component
-    * the answer is the size of the longest connected component
+    * reduce the problem to a graph and perfrom depth first search to get each chain
+    * the answer is the size of the longest chain
     * 
     * Complexity:
     * Time Complexity : O(n)
@@ -14,37 +13,22 @@ public:
     {
         unordered_set<int> s(nums.begin(), nums.end());
         int maxLength{};
-        GRAPH graph;
         
-        // build the graph
+        // get the length of each chain
         for(int elem : s)
-            if(s.count(elem+1))
-                addDirectedEdge(graph, elem, elem+1);
-        
-        // get the length of each connected component
-        for(int elem : s)
-        {
             if(!s.count(elem-1))
-            {
-                int length = dfs(graph, elem);
-                maxLength = max(maxLength, length);
-            }
-        }
+                maxLength = max(maxLength, dfs(s, elem));
         
         return maxLength;
     }
     
-    int dfs(GRAPH &graph, int node)
+    int dfs(unordered_set<int>& s, int node)
     {
         int length{1};
-        while(graph.count(node))
-            node = graph[node], length++;
+        
+        while(s.count(node+1))
+            node++, length++;
         
         return length;
-    }
-
-    void addDirectedEdge(GRAPH &graph, int from, int to)
-    {
-        graph[from] = to;
     }
 };
