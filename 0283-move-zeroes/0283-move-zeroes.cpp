6dec38daf4169604587c3cf5412b1b188b3c6d11
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-07 15:38:40 +0200

    Time: 64 ms (29.32%), Space: 19.3 MB (57.87%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -7,13 +7,8 @@ public:
     */
     void moveZeroes(vector<int>& nums) 
     {
-        int insertPos{};
-        
-        for(int i = 0 ; i < (int)nums.size() ; i++)
+        for(int insertPos = 0, i = 0 ; i < (int)nums.size() ; i++)
             if(nums[i] != 0)
-                nums[insertPos++] = nums[i];
-        
-        for(int i = insertPos ; i < (int)nums.size() ; i++)
-            nums[i] = 0;
+                swap(nums[insertPos++], nums[i]);
     }
 };
