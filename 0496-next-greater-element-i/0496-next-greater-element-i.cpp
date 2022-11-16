07b00e0cbc2947a4437eb04ef03828ae75ebaf5a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-16 15:11:31 +0200

    Time: 29 ms (19.29%), Space: 8.5 MB (87.41%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,14 +3,15 @@ public:
     /* 
     * Complexity:
     * Time Complexity : O(n^2)
-    * Space Complexity : O(1)
+    * Space Complexity : O(1) >> neglecting the output space
     */
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
-        vector<int> result;
+        vector<int> result((int)nums1.size(), -1);
+        
         for(int i =  0 ; i < (int)nums1.size() ; i++)
         {
-            bool foundElem{}, foundGreater{};
+            bool foundElem{};
             for(int j = 0 ; j < (int)nums2.size() ; j++)
             {
                 if(nums1[i] == nums2[j])
@@ -18,14 +19,10 @@ public:
                 
                 if(foundElem && nums2[j] > nums1[i])
                 {
-                    result.push_back(nums2[j]);
-                    foundGreater = 1;
+                    result[i] = nums2[j];
                     break;
                 }
-                    
             }
-            if(!foundGreater)
-                result.push_back(-1);
         }
         return result;
     }
