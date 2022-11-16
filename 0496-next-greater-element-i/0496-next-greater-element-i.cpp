e1daf18b2d5bd0b88097982e315b4dff3b331cb4
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-16 15:22:43 +0200

    Time: 16 ms (36.19%), Space: 8.8 MB (70.08%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     /* 
     * Complexity:
-    * Time Complexity : O(n^2)
-    * Space Complexity : O(1) >> neglecting the output space
+    * Time Complexity : O(m+n)
+    * Space Complexity : O(m)
     */
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
+        unordered_map<int, int> valToIdx;
+        stack<int> s;
         vector<int> result((int)nums1.size(), -1);
         
-        for(int i =  0 ; i < (int)nums1.size() ; i++)
+        for(int i = 0 ; i < (int)nums1.size() ; i++)
+            valToIdx[nums1[i]] = i;
+        
+        for(int i = 0 ; i < (int)nums2.size() ; i++)
         {
-            bool foundElem{};
-            for(int j = 0 ; j < (int)nums2.size() ; j++)
-            {
-                if(nums1[i] == nums2[j])
-                    foundElem = 1;
-                
-                if(foundElem && nums2[j] > nums1[i])
-                {
-                    result[i] = nums2[j];
-                    break;
-                }
-            }
+            while(!s.empty() && nums2[i] > s.top())
+                result[valToIdx[s.top()]] = nums2[i], s.pop();
+            
+            if(valToIdx.count(nums2[i]))
+                s.push(nums2[i]);
         }
+        
         return result;
     }
 };
