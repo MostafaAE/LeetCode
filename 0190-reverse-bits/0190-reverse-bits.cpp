b7ff02ddc406d08e3096cb917ae233808da9a58a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-17 21:57:29 +0200

    Time: 10 ms (11.28%), Space: 6 MB (50.81%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,30 +2,15 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) 
     {
-        for(int i = 0 ; i < 16 ; i++)
+        uint32_t result{};
+        for(int i = 0 ; i < 32 ; i++)
         {
-            int curLeftBit = getBit(n, i);
-            int curRightBit = getBit(n, 31-i);
-            
-            curLeftBit ? setBit1(n, 31-i) : setBit0(n, 31-i);
-            curRightBit ? setBit1(n, i) : setBit0(n, i);
+            int curMostRight = n&1;
+            result <<= 1;
+            result |= curMostRight;
+            n >>= 1;
         }
 
-        return n; 
-    }
-    
-    int getBit(uint32_t n, int idx)
-    {
-        return ((n >> idx)&1) == 1;
-    }
-    
-    void setBit1(uint32_t &n, int idx)
-    {
-        n |= (1 << idx);
-    }
-    
-    void setBit0(uint32_t &n, int idx)
-    {
-        n &= ~(1 << idx);
+        return result; 
     }
 };
