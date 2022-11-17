1c155ab6dcfba98538c15fb33594b5f0e4561f40
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-17 22:07:22 +0200

    Time: 3 ms (78.46%), Space: 6 MB (9.70%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+    /* 
+    * Approach:
+    * Bit manipulation
+    * 
+    * Complexity:
+    * Time Complexity : O(1)
+    * Space Complexity : O(1)
+    */
     uint32_t reverseBits(uint32_t n) 
     {
         uint32_t result{};
         for(int i = 0 ; i < 32 ; i++)
         {
-            int curMostRight = n&1;
             result <<= 1;
-            result |= curMostRight;
+            result |= (n&1);
             n >>= 1;
         }
 
