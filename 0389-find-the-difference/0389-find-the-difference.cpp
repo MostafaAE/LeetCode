0284625cf681a9e5c50c74be419101bf0ee39b07
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-17 20:37:28 +0200

    Time: 7 ms (53.15%), Space: 6.7 MB (68.38%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -3,21 +3,15 @@ public:
      /* 
     * Complexity:
     * Time Complexity : O(n)
-    * Space Complexity : O(1) >> constant space (26 characters only)
+    * Space Complexity : O(1)
     */
     char findTheDifference(string s, string t) 
     {
-        unordered_map<int,int> count;
+        int sum{t[(int)t.size()-1]};
         
         for(int i = 0 ; i < (int)s.size() ; i++)
-            count[s[i]]++, count[t[i]]++;
+            sum += t[i] - s[i];
         
-        count[t[(int)t.size()-1]]++;
-        
-        for(auto &p : count)
-            if(p.second&1) // odd
-                return p.first;
-        
-        return ' ';
+        return sum;
     }
 };
