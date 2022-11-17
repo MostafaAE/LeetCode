2151c3607ad8309be22e80b0c874281c058312e2
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-17 22:25:23 +0200

    Time: 3 ms (98.98%), Space: 7.8 MB (52.58%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -7,21 +7,19 @@ public:
     */
     int romanToInt(string s) 
     {
-        int lastVal{1000};
+        int n = (int)s.size();
         int sum{};
         unordered_map<char, int> romToInt{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}
                                           , {'C', 100}, {'D', 500}, {'M', 1000}};
     
-        for(int i = 0 ; i < (int)s.size() ; i++)
+        for(int i = 0 ; i < n ; i++)
         {
             int curRomVal = romToInt[s[i]];
             
-            sum += curRomVal;
-            
-            if(curRomVal > lastVal)
-                sum -= 2*lastVal;
-            
-            lastVal = romToInt[s[i]];
+            if(i < n-1 && romToInt[s[i+1]] > curRomVal)
+                sum -= curRomVal;
+            else
+                sum += curRomVal;
         }
         
         return sum;
