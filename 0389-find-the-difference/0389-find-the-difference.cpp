f585efa5a8fb1c120cb7a11885b3d707e46a85ac
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-18 14:47:30 +0200

    Time: 0 ms (100.00%), Space: 6.6 MB (87.64%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -7,11 +7,12 @@ public:
     */
     char findTheDifference(string s, string t) 
     {
-        int sum{t[(int)t.size()-1]};
+        char diff{};
         
         for(int i = 0 ; i < (int)s.size() ; i++)
-            sum += t[i] - s[i];
+            diff ^= s[i], diff ^= t[i];
+        diff ^= t[(int)t.size()-1];
         
-        return sum;
+        return diff;
     }
 };
