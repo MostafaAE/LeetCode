eefb3efaf67711e2398bc13c0c6b4b11bc3ae4fb
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-19 18:11:14 +0200

    Time: 2130 ms (8.92%), Space: 203.7 MB (5.84%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,12 +1,12 @@
 typedef unordered_map<int, unordered_set<int>> HASHSET;
 class Solution {
 public:
-    bool canMeasureWater(int jug1, int jug2, int targetCapacity) 
+    bool canMeasureWater(int cap1, int cap2, int targetCapacity) 
     {
-        if(jug1 + jug2 == targetCapacity || jug1 == targetCapacity || jug2 == targetCapacity)
+        if(cap1 + cap2 == targetCapacity || cap1 == targetCapacity || cap2 == targetCapacity)
             return true;
         
-        else if (jug1 + jug2 < targetCapacity)
+        else if (cap1 + cap2 < targetCapacity)
             return false;
         
         queue<pair<int, int>> q;
@@ -23,9 +23,9 @@ public:
                 auto [j1, j2] = q.front();
                 q.pop();
                 
-                int m1 = min(j1, jug2 - j2), m2 = min(j2, jug1 - j1);
+                int m1 = min(j1, cap2 - j2), m2 = min(j2, cap1 - j1);
                 if(process(q, visited, 0, j2, targetCapacity) || process(q, visited, j1, 0, targetCapacity) 
-                   || process(q, visited, jug1, j2, targetCapacity) || process(q, visited, j1, jug2, targetCapacity)
+                   || process(q, visited, cap1, j2, targetCapacity) || process(q, visited, j1, cap2, targetCapacity)
                    || process(q, visited, j1 - m1, j2 + m1, targetCapacity)
                    || process(q, visited, j1 + m2, j2 - m2, targetCapacity))
                     return true;
@@ -40,8 +40,13 @@ public:
         if(j1 + j2 == targetCapacity || j1 == targetCapacity || j2 == targetCapacity)
             return true;
         
+        // optimization
+        if(j1 > j2)
+            swap(j1, j2);
+        
         if(visited[j1].insert(j2).second)
             q.push({j1, j2});
+        
         return false;
     }
 };
