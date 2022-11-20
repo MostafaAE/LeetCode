abd7b75d695acd256967fb6991e4c4fb7d96d726
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-20 16:23:09 +0200

    Time: 12 ms (78.75%), Space: 18.8 MB (56.21%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,11 +11,21 @@
  */
 class Solution {
 public:
+    /* 
+    * Approach: 
+    * depth first search
+    *
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(h) => auxiliary space for stack
+    * h could be (n) or (logn) depending on the shape of the tree
+    */
     int maxDepth(TreeNode* root) 
     {
         if(!root)
             return 0;
         
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        
     }
 };
