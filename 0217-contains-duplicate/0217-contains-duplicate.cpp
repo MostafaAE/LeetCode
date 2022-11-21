4338ff63077a4d6c639b6a3dea3d6158a66055ef
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-22 00:31:45 +0200

    Time: 173 ms (67.32%), Space: 51.2 MB (70.85%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    
-    // Time Complexity: O(n)
-    // Memory Complexity: O(n)
+    /* 
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(n)
+    */
     bool containsDuplicate(vector<int>& nums) 
     {
         unordered_set<int> s;
         
-        // O(n)
         for(int i = 0 ; i < (int)nums.size() ; i++)
-        {
-            // insert() method takes O(1).
             if(!s.insert(nums[i]).second)
                 return true;
-        }
+        
         return false;
     }
 };
