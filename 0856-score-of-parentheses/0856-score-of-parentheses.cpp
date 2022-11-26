785ca4eda81a032d51027c41ab2bd7341119bb99
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-26 19:08:03 +0200

    Time: 2 ms (48.55%), Space: 6.2 MB (45.98%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * stack
+    * 
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(n)
+    */
+    int scoreOfParentheses(string s)
+    {
+        stack<int> parser;
+        parser.push(0);
+
+        for (char c : s)
+        {
+            if (c == '(')
+                parser.push(0);
+            else
+            {
+                int last = parser.top();
+                parser.pop();
+
+                if (last == 0)
+                    last = 1;
+                else
+                    last *= 2;
+
+                int parentVal = parser.top() + last;
+                parser.pop();
+                parser.push(parentVal);
+            }
+        }
+        return parser.top();
+    }
+};
