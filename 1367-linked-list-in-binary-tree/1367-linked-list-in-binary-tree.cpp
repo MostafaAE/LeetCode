b8bc8e49c25156fea84451a1c59275d0f0faea12
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-11-26 23:35:38 +0200

    Time: 86 ms (18.76%), Space: 32.1 MB (63.04%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    /* 
+    * Approach:
+    * Iterate over the tree using DFS checking if all the elements in the linked list starting from the head
+    * correspond to some downward path in the binary tree.
+    * 
+    * Complexity:
+    * Time Complexity: O(n * min(L,h)) where L is the list length and h is the tree height
+    * Space Complexity: O(h) => auxiliary space for stack
+    * h could be (n) or (logn) depending on the shape of the tree
+    */
+    bool dfs(ListNode* head, TreeNode* root)
+    {
+        if(!head)
+            return 1;
+
+        if(!root)
+            return 0;
+        
+        return (head->val == root->val) && (dfs(head->next, root->left) || dfs(head->next, root->right));
+    }
+    
+    bool isSubPath(ListNode* head, TreeNode* root) 
+    {
+        if(!head)
+            return 1;
+        if(!root)
+            return 0;
+        
+        return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+};
