27c35512f1fa355d47314b0efd333e068cd0a0a1
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-12-04 18:19:16 +0200

    Time: 27 ms (99.32%), Space: 18.3 MB (94.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    /* 
+    * Complexity:
+    * Time Complexity : O(m*nlogn) where m is number of words, and n is average length of a word
+    * Space Complexity : O(m*n)
+    */
     vector<vector<string>> groupAnagrams(vector<string>& strs) 
     {
         vector<vector<string>> result;
@@ -9,6 +14,7 @@ public:
         for(int i = 0 ; i < (int)strs.size() ; i++)
         {
             string cpy = strs[i];
+            // O(nlogn)
             sort(cpy.begin(), cpy.end());
             
             if(!anagramToIdx.count(cpy))
