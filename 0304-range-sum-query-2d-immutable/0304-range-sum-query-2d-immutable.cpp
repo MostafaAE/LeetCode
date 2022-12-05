b5ba48c89c61c30001b8d7d46e8e30e58ec63eba
MostafaAE <mostafa.aymanae@gmail.com>
2022-12-05 22:08:23 +0200

    Time: 438 ms (90.14%), Space: 142.4 MB (82.64%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,33 +1,27 @@
 class NumMatrix {
 private:
-vector<vector<int>> mat;
+vector<vector<int>> prefix;
 public:
     NumMatrix(vector<vector<int>>& matrix) 
     {
-        mat = matrix;
+        int row{(int)matrix.size()}, col{(int)matrix[0].size()};
+        prefix = vector<vector<int>>(row+1, vector<int>(col+1, 0));
         
         // accumulate each row
-        for(int i = 0 ; i < (int)mat.size() ; i++)
-            for(int j = 1 ; j < (int)mat[0].size() ; j++)
-                mat[i][j] += mat[i][j-1];
-        
+        for(int i = 1 ; i <= row ; i++)
+            for(int j = 1 ; j <= col ; j++)
+                prefix[i][j] += matrix[i-1][j-1] + prefix[i][j-1];
+
         // accumulate each column
-        for(int j = 0 ; j < (int)mat[0].size() ; j++)
-            for(int i = 1 ; i < (int)mat.size() ; i++)
-                mat[i][j] += mat[i-1][j];
+        for(int j = 1 ; j <= col ; j++)
+            for(int i = 1 ; i <= row ; i++)
+                prefix[i][j] += prefix[i-1][j];
     }
     
     // O(1)
     int sumRegion(int row1, int col1, int row2, int col2) 
     {
-        if(row1 == 0 && col1 == 0)
-            return mat[row2][col2];
-        else if(row1 == 0)
-            return mat[row2][col2] - mat[row2][col1-1];
-        else if(col1 == 0)
-            return mat[row2][col2] - mat[row1-1][col2];
-        else
-            return mat[row2][col2] - mat[row1-1][col2] - mat[row2][col1-1] + mat[row1-1][col1-1];
+        return prefix[row2+1][col2+1] - prefix[row1][col2+1] - prefix[row2+1][col1] + prefix[row1][col1];
     }
 };
 
