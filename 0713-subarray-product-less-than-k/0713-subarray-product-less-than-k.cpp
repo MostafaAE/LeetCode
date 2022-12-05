59eab94c649cf8d49e1cd7bc0b1ed1902c61d956
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-12-05 03:30:48 +0200

    Time: 223 ms (31.92%), Space: 61.2 MB (71.29%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * Sliding Windows
+    * 
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(1)
+    */
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) 
+    {
+        if(k <= 1)
+            return 0;
+        
+        int count{}, left{}, product{1};
+        
+        for(int right = 0 ; right < (int)nums.size() ; right++)
+        {
+            product *= nums[right];
+            while(product >= k)
+                product /= nums[left++];
+            
+            count += right - left + 1;
+        }
+        
+        return count;
+    }
+};
