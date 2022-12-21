ce3bba91108260225e341a5b51cb9da9c6100959
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2022-12-22 00:00:59 +0200

    Time: 3 ms (98.62%), Space: 8.7 MB (32.46%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -7,6 +7,14 @@ public:
     /*
      * Approach:
      * Dynamic Programming Memoization
+     * 
+     * Direct Consecutive range pattern
+     * We want to find a good blocks split
+     *
+     * dp(idx)
+     *  Try all consecutive ranges as long as total width <= shelfWidth
+     *      For each choice [idx, k] subarray, recurse on the remaining dp(k+1)
+     *		The choice cost is the max height in the subarray [idx, k]
      *
      * Complexity:
      * Time Complexity : O(N^2)
