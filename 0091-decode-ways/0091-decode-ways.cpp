ea999d6d732da5381c656825415b0c4a0e8f1917
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-01-10 02:21:00 +0200

    Time: 4 ms (47.50%), Space: 6.1 MB (75.81%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    static const int MAX = 100 + 1;
+    int memory[MAX];
+    string str;
+public:
+    /*
+     * Approach:
+     * Dynamic Programming Memoization
+     * 
+     * Counting DP
+     *
+     * Complexity:
+     * Time Complexity : O(N)
+     * Space Complexity : O(N)
+     */
+    int numDecodings(string s) 
+    {
+        str = s;
+        memset(memory, -1, sizeof(memory));
+        return decode(0);
+    }
+    
+    int decode(int idx)
+    {
+        // one valid way
+        if(idx >= (int)str.size())
+            return 1;
+        
+        // invalid (no ways)
+        if(str[idx] == '0')
+            return 0;
+        
+        int &ret = memory[idx];
+        if(ret != -1)
+            return ret;
+        
+        // 2 cases: 1 digit or 2 digits
+        ret = decode(idx + 1);
+        if(idx < (int)str.size() - 1 && (str[idx] == '1' || str[idx] == '2' && str[idx+1] <= '6'))
+            ret += decode(idx + 2);
+        
+        return ret;
+    }
+};
