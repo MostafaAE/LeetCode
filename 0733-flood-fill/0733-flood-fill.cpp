53ba0c8d6527f0f936e0b972deb837749ec0527b
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-01-11 09:49:15 +0200

    Time: 14 ms (58.00%), Space: 14 MB (50.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,38 +1,41 @@
+typedef vector<vector<int>> GRAPH;
 class Solution {
 private:
-    //direction arrays
-    // Delta for: up, right, down, left
+    // direction arrays
     int dr[4]{-1, 1, 0, 0};
     int dc[4]{0, 0, -1, 1};
 public:
     /* 
     * Approach:
-    * depth first search
+    * Depth-First Search
     * 
     * Complexity:
     * Time Complexity : O(N) => O(N + E) , N = RxC E = 4N => O(5N)
     * Space Complexity : O(N) => auxiliary space for stack
     */
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    vector<vector<int>> floodFill(GRAPH& image, int sr, int sc, int color) 
+    {
+        if(image[sr][sc] != color)
+            dfs(image, sr, sc, color);
+        
+        return image;
+    }
+    
+    void dfs(GRAPH& image, int sr, int sc, int color)
     {
         int oldColor = image[sr][sc];
-        if(oldColor == color)
-            return image;
-        
         image[sr][sc] = color;
         
         for(int d = 0 ; d < 4 ; d++)
         {
             int nr = sr + dr[d], nc = sc + dc[d];
             if(isValid(nr, nc, image) && image[nr][nc] == oldColor)
-                floodFill(image, nr ,nc, color);    
+                dfs(image, nr, nc, color);
         }
-        
-        return image;
     }
     
     // Return true if this position INSIDE the 2D matrix
-    bool isValid(int nr, int nc, vector<vector<int>>& image)
+    bool isValid(int nr, int nc, GRAPH& image)
     {
         if(nr < 0 || nr >= (int)image.size() || nc < 0 || nc >= (int)image[0].size())
             return false;
