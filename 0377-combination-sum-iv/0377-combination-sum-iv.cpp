6b677218764f6e0f5d96d2594fccf71c7e4e8f71
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-01-11 10:49:52 +0200

    Time: 4 ms (47.48%), Space: 6.6 MB (69.46%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
-    static const int MAXIDX = 200 + 1, MAXAMOUNT = 1000 + 1;
-    int memory[MAXIDX][MAXAMOUNT];
+    static const int MAX = 1000 + 1;
+    int memory[MAX];
     vector<int> nums;
     
 public:
@@ -12,17 +12,17 @@ public:
      * Counting DP
      *
      * Complexity:
-     * Time Complexity : O(MN)
-     * Space Complexity : O(MN)
+     * Time Complexity : O(MN) 
+     * Space Complexity : O(MN) where M is the max target, and N is the max index
      */
     int combinationSum4(vector<int>& _nums, int target) 
     {
         memset(memory, -1 , sizeof(memory));
         nums = _nums;
-        return combSum(0, target);
+        return combSum(target);
     }
     
-    int combSum(int idx, int target)
+    int combSum(int target)
     {
         // not valid
         if(target < 0)
@@ -32,14 +32,13 @@ public:
         if(target == 0)
             return 1;
         
-        int &ret = memory[idx][target];
+        int &ret = memory[target];
         if(ret != -1)
             return ret;
         
-        
         ret = 0;
         for(int i = 0 ; i < (int)nums.size() ; i++)
-            ret += combSum(i, target - nums[i]);
+            ret += combSum(target - nums[i]);
     
         return ret;
     }
