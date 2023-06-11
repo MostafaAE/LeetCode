75e0ebe5c63fac92c6a68f70a5f31ed3f37b0301
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-11 17:08:00 +0300

    Time: 50 ms (75.78%), Space: 10.3 MB (88.63%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,7 +1,7 @@
-class Solution {
-private:
-const static int MAX = 1000 + 1;
+const int MAX = 1000 + 1;
 int memory[MAX][MAX];
+string text1, text2;
+class Solution {
 public:
     /* 
     * Approach:
@@ -11,15 +11,18 @@ public:
     * Time Complexity : O(NM)
     * Space Complexity : O(NM) where N is text1.length, and M is text2.length
     */
-    int longestCommonSubsequence(string text1, string text2) 
+    int longestCommonSubsequence(string text_1, string text_2) 
     {
         memset(memory, -1, sizeof(memory));
-        return LCS(text1, text2, 0, 0);
+        text1 = text_1;
+        text2 = text_2;
+        
+        return LCS(0, 0);
     }
     
-    int LCS(string& text1, string& text2, int idx1, int idx2)
+    int LCS(int idx1, int idx2)
     {
-        if(idx1 == (int)text1.size() || idx2 == (int)text2.size())
+        if(idx1 >= (int)text1.size() || idx2 >= (int)text2.size())
             return 0;
         
         int &ret = memory[idx1][idx2];
@@ -28,14 +31,13 @@ public:
         
         // pick from both
         if(text1[idx1] == text2[idx2])
-            return ret = 1 + LCS(text1, text2, idx1 + 1, idx2 + 1);
-            
+            return ret = 1 + LCS(idx1 + 1, idx2 + 1);
         // leave from text1
-        int choice1 = LCS(text1, text2, idx1 + 1, idx2);
-        // leave from text2
-        int choice2 = LCS(text1, text2, idx1, idx2 + 1);
-
-        return ret = max(choice1, choice2);
+        int choice1 = LCS(idx1 + 1, idx2);
         
+        // leave from text2
+        int choice2 = LCS(idx1, idx2 + 1);
+        
+        return ret = max(choice1, choice2);
     }
 };
