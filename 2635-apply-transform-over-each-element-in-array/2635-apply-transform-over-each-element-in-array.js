a631e53cfe2616f7b88cd7391d85106e1d91f96e
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-12 23:36:14 +0300

    Time: 60 ms (47.22%), Space: 41.7 MB (78.67%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let newArr = [];
+    for(let i = 0 ; i < arr.length ; i++)
+        newArr.push(fn(arr[i], i));
+    
+    return newArr;
+};
