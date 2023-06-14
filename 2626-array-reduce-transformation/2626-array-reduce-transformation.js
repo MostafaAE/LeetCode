e58f5341923307cc5507f97015efed6c444af8b1
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-14 20:45:55 +0300

    Time: 61 ms (60.77%), Space: 44 MB (18.29%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let res = init;
+    
+    for(const num of nums)
+        res = fn(res, num);
+    
+    return res;
+};
