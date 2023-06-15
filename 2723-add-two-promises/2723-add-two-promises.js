f379ab477cc960994ab5d239a6ebf39942b537de
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-15 14:10:38 +0300

    Time: 65 ms (72.70%), Space: 42.2 MB (35.75%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,10 +4,13 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const a = await promise1;
-    const b = await promise2;
-    
-    return new Promise((resolve) => resolve(a+b));
+    try{
+        const [a, b] = await Promise.all([promise1, promise2]);
+        return a + b;
+    }
+    catch(err){
+        throw err;
+    }
     
 };
 
