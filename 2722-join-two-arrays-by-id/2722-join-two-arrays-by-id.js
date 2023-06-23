9f7033c7d6f3a46c6d9c5f9eb0a96a26fd625d23
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-23 21:56:14 +0300

    Time: 416 ms (38.25%), Space: 122.5 MB (17.82%) - LeetHub

--- 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -4,19 +4,9 @@
  * @return {Array}
  */
 var join = function (arr1, arr2) {
-  const map = new Map();
+  const map = {};
 
-  arr1.forEach(el => map.set(el.id, el));
+  [...arr1, ...arr2].forEach(e => (map[e.id] = { ...map[e.id], ...e }));
 
-  arr2.forEach(el => {
-    if (map.has(el.id)) {
-      const obj = map.get(el.id);
-      Object.entries(el).forEach(entry => {
-        const [key, value] = entry;
-        obj[key] = value;
-      });
-    } else map.set(el.id, el);
-  });
-
-  return [...map.values()].sort((a, b) => a.id-b.id);
+  return [...Object.values(map)];
 };
