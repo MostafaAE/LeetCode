2b3afc3c08d2f34f26bfa635df3458563805fb07
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-26 04:30:22 +0300

    Time: 1797 ms (5.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,13 @@
+SELECT 
+    customer_id
+FROM 
+    customer
+GROUP BY 
+    customer_id
+HAVING 
+    COUNT(DISTINCT product_key) = (
+    SELECT 
+        COUNT(*)
+    FROM
+        product
+);
