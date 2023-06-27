e7f623990c71f528ce9f3fe63fc576b1e1965e16
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-27 17:33:32 +0300

    Time: 1333 ms (25.40%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,22 +1,7 @@
 SELECT 
-    e1.employee_id,
-    (
-    CASE
-        WHEN 
-            COUNT(e1.department_id) = 1
-        THEN 
-            e1.department_id
-        ELSE (
-            SELECT 
-                department_id
-            FROM 
-                employee e2
-            WHERE
-                e1.employee_id = e2.employee_id AND primary_flag ='Y'
-        )
-    END
-    ) AS department_id
+    employee_id,
+    ABS(MAX(CASE WHEN primary_flag = 'Y' THEN department_id ELSE -department_id END)) AS department_id
 FROM
-    employee e1
+    employee
 GROUP BY 
-    e1.employee_id;
+    employee_id;
