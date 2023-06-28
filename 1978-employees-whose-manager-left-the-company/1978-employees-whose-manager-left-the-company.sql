a15d980bac07ec538cb5c9f0f0bb69962d8ea20c
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-06-28 19:16:12 +0300

    Time: 890 ms (18.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,11 @@
+SELECT 
+    employee_id
+FROM
+    employees
+WHERE
+    salary < 30000 AND manager_id NOT IN (
+        SELECT DISTINCT employee_id
+        FROM employees
+    )
+ORDER BY 
+    employee_id;
