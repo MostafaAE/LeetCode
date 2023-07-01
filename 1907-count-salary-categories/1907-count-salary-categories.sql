ec85765d04b7e6c0cb1f91b5038dc880f636ae06
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-07-01 22:25:12 +0300

    Time: 3013 ms (18.42%), Space: 0B (100.00%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,27 @@
+SELECT 
+    'Low Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM
+    accounts
+WHERE
+    income < 20000
+    
+UNION
+
+SELECT 
+    'Average Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM
+    accounts
+WHERE
+    income >= 20000 AND income <= 50000
+    
+UNION
+
+SELECT 
+    'High Salary' AS category,
+    COUNT(*) AS accounts_count
+FROM
+    accounts
+WHERE
+    income > 50000;
