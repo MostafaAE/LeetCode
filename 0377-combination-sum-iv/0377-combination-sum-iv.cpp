82c4446445bd6f90b8693443f28a0d30ff243cb4
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-07-03 02:03:26 +0300

    Time: 5 ms (38.86%), Space: 6.6 MB (45.67%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,9 +1,8 @@
+const int MAX = 1000 + 1;
+int memory[MAX];
+vector<int> numsg;
+
 class Solution {
-private:
-    static const int MAX = 1000 + 1;
-    int memory[MAX];
-    vector<int> nums;
-    
 public:
     /*
      * Approach:
@@ -13,32 +12,32 @@ public:
      *
      * Complexity:
      * Time Complexity : O(MN) 
-     * Space Complexity : O(MN) where M is the max target, and N is the max index
+     * Space Complexity : O(M) where M is the max target, and N is the max index
      */
-    int combinationSum4(vector<int>& _nums, int target) 
+    int combinationSum4(vector<int>& nums, int target) 
     {
         memset(memory, -1 , sizeof(memory));
-        nums = _nums;
+        numsg = nums;
         return combSum(target);
     }
     
-    int combSum(int target)
+    int combSum(int remaining)
     {
         // not valid
-        if(target < 0)
+        if(remaining < 0)
             return 0;
         
         // valid way
-        if(target == 0)
+        if(remaining == 0)
             return 1;
         
-        int &ret = memory[target];
+        int &ret = memory[remaining];
         if(ret != -1)
             return ret;
         
         ret = 0;
-        for(int i = 0 ; i < (int)nums.size() ; i++)
-            ret += combSum(target - nums[i]);
+        for(int i = 0 ; i < (int)numsg.size() ; i++)
+            ret += combSum(remaining - numsg[i]);
     
         return ret;
     }
