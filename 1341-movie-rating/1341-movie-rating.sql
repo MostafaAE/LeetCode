64b10ab578f122e0f49e7931c72a1aaa886773ed
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-07-29 04:13:38 +0300

    Time: 2794 ms (53.42%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,39 +1,28 @@
-SELECT
+(SELECT
     name as results
 FROM
-(
-    SELECT
-        name,
-        COUNT(*) as count
-    FROM
-        users u
-    JOIN
-        movierating mr ON u.user_id = mr.user_id
-    GROUP BY
-        u.user_id
-    ORDER BY 
-        count DESC, name
-    LIMIT 1
-) as user
+    users u
+JOIN
+    movierating mr ON u.user_id = mr.user_id
+GROUP BY
+    u.user_id
+ORDER BY 
+    COUNT(*) DESC, name
+LIMIT 1)
+
     
 UNION ALL
 
-SELECT
+(SELECT
     title as results
 FROM
-(
-    SELECT
-        title,
-        AVG(rating) as avg
-    FROM
-        movies m
-    JOIN
-        movierating mr ON m.movie_id = mr.movie_id
-    WHERE
-        created_at >= '2020-02-01' AND created_at <= '2020-02-29'
-    GROUP BY
-        m.movie_id
-    ORDER BY 
-        avg DESC, title
-    LIMIT 1
-) as movie;
+    movies m
+JOIN
+    movierating mr ON m.movie_id = mr.movie_id
+WHERE
+    MONTH(created_at) = '02' AND YEAR(created_at) = '2020'
+GROUP BY
+    m.movie_id
+ORDER BY 
+    AVG(rating) DESC, title
+LIMIT 1);
