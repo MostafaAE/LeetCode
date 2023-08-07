606a17f747a9c41ef1ae5e7f1157252ec2d81aeb
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-08-07 21:34:56 +0300

    Time: 673 ms (44.32%), Space: 9.5 MB (56.41%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,58 @@
+vector<int> nums;
+int target, k;
+class Solution {
+public:
+    /* 
+    * Approach:
+    * Optimized Backtracking (Each partition searches for its numbers)
+    * 
+    * Complexity:
+    * Time Complexity : O(K*2^N)
+    * Space Complexity : O(N)
+    */
+    bool canPartitionKSubsets(vector<int>& nums_, int k_) 
+    {
+        nums = nums_;
+        k = k_;
+        int n{(int)nums.size()};
+        vector<bool> selected(n, false);
+        
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum % k == 0 && n >= k)
+        {
+            target = sum / k;
+            return tryPartition(0, 0, 0, selected);
+        }
+        
+        return false;
+    }
+    
+    bool tryPartition(int partitionIdx, int numberIdx, int curPartitionSum, vector<bool> &selected)
+    {
+        if(numberIdx >= (int)nums.size())
+        {
+            if(curPartitionSum == target && partitionIdx == k - 1)
+                return true;
+            
+            return false;
+        }
+        
+        if(curPartitionSum == target)
+        {
+            return tryPartition(partitionIdx + 1, 0, 0, selected);
+        }
+        
+        // pick
+        if(!selected[numberIdx] && nums[numberIdx] + curPartitionSum <= target)
+        {
+            selected[numberIdx] = true;
+            if(tryPartition(partitionIdx, numberIdx + 1, curPartitionSum + nums[numberIdx], selected))
+                return true;
+            selected[numberIdx] = false;
+        }
+        
+        // leave
+        return tryPartition(partitionIdx, numberIdx + 1, curPartitionSum, selected);
+        
+    }
+};
