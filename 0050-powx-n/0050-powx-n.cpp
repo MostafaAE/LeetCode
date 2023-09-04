72e1cfc5e178dca5cfd83b756d96981a08ba8d8c
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-09-04 17:50:44 +0300

    Time: 0 ms (100.00%), Space: 6 MB (60.08%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     /* 
+    * Approach: 
+    * Divide and conquer
+    *
     * Complexity:
-    * Time Complexity : O(logn)
-    * Space Complexity : O(logn) => auxiliary space for stack
+    * Time Complexity : O(log(n))
+    * Space Complexity : O(log(n)) => auxiliary space for stack
     */
-    double myPow(double x, int n) 
+    double myPow(double x, int n)
     {
         if(n < 0)
-            x = 1/x;
-        
-        long long num = llabs(n);
-        
-        if(num == 0)
+            x = 1 / x;
+
+        return pow(x, llabs(n));
+    }
+    
+    double pow(double x, long long n)
+    {
+        if(!n)
             return 1;
         
-        double pow = myPow(x, num/2);
-
-        // odd
-        if(num&1)
-            return x * pow * pow;
-        else
-            return pow * pow;
+        double p = pow(x, n / 2);
+        
+        // odd power
+        if(n&1)
+            return x * p * p;
+        
+        // even power
+        return p * p;
     }
 };
