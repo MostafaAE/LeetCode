0339c4bba948fd5f3d2b39ce906747154cdbc215
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-09-22 23:11:49 +0300

    Time: 73 ms (66.67%), Space: 9.3 MB (81.40%) - LeetHub

--- 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.cpp
+++ 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     /* 
     * Approach:
-    * breadth first search
+    * Breadth first search
     * 
     * Complexity:
     * Total number of states = O(M) = 1001
@@ -11,33 +11,33 @@ public:
     */
     int minimumOperations(vector<int>& nums, int start, int goal) 
     {
-        vector<bool> visited(1001,0);
+        int n{(int)nums.size()};
+        
+        vector<bool> visited(1001, 0);
         queue<int> q;
         
+        visited[start] = true;
         q.push(start);
-        visited[start] = 1;
         int level = 0;
         
         while(!q.empty())
         {
             int sz = q.size();
-            
             while(sz--)
             {
                 int cur = q.front();
                 q.pop();
-
-                for(int i = 0 ; i < (int)nums.size() ; i++)
+                
+                for(int i = 0 ; i < n ; i++)
                 {
-                    int ops[3] = {cur+nums[i], cur-nums[i], cur^nums[i]};
-                    
-                    for(int j = 0 ; j < 3 ; j++)
+                    int results[] = {cur + nums[i], cur - nums[i], cur ^ nums[i]};
+
+                    for(int res : results)
                     {
-                        if(ops[j] == goal)
-                            return level+1;
-                        
-                        if(0 <= ops[j] && ops[j] <= 1000 && !visited[ops[j]])
-                            q.push(ops[j]), visited[ops[j]] = 1;
+                        if(res == goal)
+                            return level + 1;
+                        if(res >= 0 && res <= 1000 && !visited[res])
+                            visited[res] = true, q.push(res);
                     }
                 }
             }
@@ -46,5 +46,6 @@ public:
         }
         
         return -1;
+        
     }
 };
