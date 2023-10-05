4cb4bc84ce6dbd29c7703d69d5cea2fce285ffd3
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-10-05 23:19:25 +0300

    Time: 40 ms (30.47%), Space: 18.4 MB (58.27%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,78 +1,84 @@
 class Solution {
 private:
-int dr[4]{1, -1, 0, 0};
-int dc[4]{0, 0, 1, -1};
-    
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 public:
     /* 
     * Approach:
     * Breadth first search
     * 
     * Complexity:
-    * Time Complexity : O(m*n)
-    * Space Complexity : O(m*n)
+    * Time Complexity : O(MN)
+    * Space Complexity : O(MN)
     */
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) 
     {
-        int m{(int)heights.size()}, n{(int)heights[0].size()};
+        int n{(int)heights.size()}, m{(int)heights[0].size()};
+        vector<vector<bool>> touchesPacific(n, vector<bool>(m));
+        vector<vector<bool>> touchesAtlantic(n, vector<bool>(m));
         
-        vector<vector<bool>> atlanticReachable(m, vector<bool>(n));
-        vector<vector<bool>> pacificReachable(m, vector<bool>(n));
-        
-        vector<pair<int,int>> pacificStartings, atlanticStartings;
-        // get 1st/last row/col as starting points
-        for (int r = 0; r < m ; r++)
-        {
-            pacificStartings.push_back({r, 0});
-            atlanticStartings.push_back({r, n-1});
-        }
-        for (int c = 0; c < n ; c++)
-        {
-            pacificStartings.push_back({0, c});
-            atlanticStartings.push_back({m-1, c});
-        }
-        
-        bfs(heights, pacificReachable, pacificStartings);
-        bfs(heights, atlanticReachable, atlanticStartings);
-        
-        // building the result
         vector<vector<int>> result;
-        for(int r = 0 ; r < m ; r++)
-            for(int c = 0 ; c < n ; c++)
-                if(atlanticReachable[r][c] && pacificReachable[r][c])
-                    result.push_back({r,c});
-    
+        
+        // Prepare pacific and atlantic queue
+        queue<pair<int, int>> pacificQueue;
+        queue<pair<int, int>> atlanticQueue;
+        
+        for(int col = 0 ; col < m ; col++)
+        {
+            pacificQueue.push({0, col});
+            atlanticQueue.push({n-1, col});
+            touchesPacific[0][col] = true;
+            touchesAtlantic[n-1][col] = true;
+        }
+        for(int row = 0 ; row < n ; row++)
+        {
+            pacificQueue.push({row, 0});
+            atlanticQueue.push({row, m-1});
+            touchesPacific[row][0] = true;
+            touchesAtlantic[row][m-1] = true;
+        }
+        
+        bfs(pacificQueue, heights, touchesPacific);
+        bfs(atlanticQueue, heights, touchesAtlantic);
+            
+        for(int row = 0 ; row < n ; row++)
+        {
+            for(int col = 0 ; col < m ; col++)
+                if(touchesPacific[row][col] && touchesAtlantic[row][col])
+                    result.push_back({row, col});
+        }
+        
         return result;
     }
-    
-    void bfs(vector<vector<int>>& heights, vector<vector<bool>> &visited, vector<pair<int,int>> &startings)
+            
+    void bfs(queue<pair<int, int>>& queue, vector<vector<int>>& heights, vector<vector<bool>>& visited)
     {
-        queue<pair<int, int>> q;
-        for(auto &p : startings)
-            q.push(p), visited[p.first][p.second] = 1;
-        
-        while(!q.empty())
+        while(!queue.empty())
         {
-            int sz = q.size();
+            int sz{(int)queue.size()};
             while(sz--)
             {
-                auto &p = q.front();
-                int r = p.first, c = p.second;
-                q.pop();
+                auto [r, c] = queue.front();
+                queue.pop();
                 
-                for (int d = 0; d < 4; ++d) 
+                for(int d = 0; d < 4 ; d++)
                 {
-				    int nr = r + dr[d], nc = c + dc[d];
-                    if(isValid(nr, nc, heights) && !visited[nr][nc] && heights[nr][nc] >= heights[r][c])
-                        q.push({nr, nc}), visited[nr][nc] = 1;
+                    int nr = r + dx[d];
+                    int nc = c + dy[d];
+                    if(isValid(nr, nc, heights) && heights[nr][nc] >= heights[r][c] && !visited[nr][nc])
+                    {
+                        visited[nr][nc] = true;
+                        queue.push({nr, nc});
+                    }
                 }
             }
         }
+        
     }
-    
-    bool isValid(int nr, int nc, vector<vector<int>> &grid)
+            
+    bool isValid(int r, int c, vector<vector<int>>& heights)
     {
-        if (nr < 0 || nr >= (int)grid.size() || nc < 0 || nc >= (int)grid[0].size())
+        if(r < 0 || r >= heights.size() || c < 0 || c >= heights[0].size())
             return false;
         return true;
     }
