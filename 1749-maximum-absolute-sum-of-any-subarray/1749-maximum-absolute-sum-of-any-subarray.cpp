68a1759d94be1269b5165d4dbee2ff8aab89cbe9
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-12-10 21:34:47 +0200

    Time: 57 ms (18.29%), Space: 41.8 MB (18.53%) - LeetHub

---
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    /* 
+    * Approach:
+    * Get both the maximum sum subarray and the minimum sum subarray and return the max absolute of them
+    * Note: use Kadane's algorithm to get the maximum sum and minumim sum subarray to make the alogithm O(n)
+    *
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(1)
+    */
+    int maxAbsoluteSum(vector<int>& nums) {
+        return max(abs(maxSum(nums)), abs(minSum(nums)));
+    }
+    
+    int maxSum(vector<int>& nums)
+    {
+        int maxSoFar{}, maxSum{};
+        
+        for(int n : nums)
+        {
+            maxSoFar += n;
+            maxSum = max(maxSum , maxSoFar);
+            
+            if(maxSoFar < 0)
+                maxSoFar = 0;
+        }
+        return maxSum;
+    }
+    
+    int minSum(vector<int>& nums)
+    {
+        int minSoFar{}, minSum{};
+        
+        for(int n : nums)
+        {
+            minSoFar += n;
+            minSum = min(minSum , minSoFar);
+            
+            if(minSoFar > 0)
+                minSoFar = 0;
+        }
+        return minSum;
+    }
+};
