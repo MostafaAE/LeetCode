6a5bb08e03110417b17332c63a17aeca39f5a525
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2023-12-12 20:18:15 +0200

    Time: 12 ms (92.64%), Space: 19.6 MB (65.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     /* 
+    * Approach:
+    * Two pointers technique
+    * 
     * Complexity:
     * Time Complexity : O(n)
     * Space Complexity : O(1)
     */
     void moveZeroes(vector<int>& nums) 
     {
-        for(int insertPos = 0, i = 0 ; i < (int)nums.size() ; i++)
-            if(nums[i] != 0)
-                swap(nums[insertPos++], nums[i]);
+        int left{}, n{(int)nums.size()};
+        
+        for(int right = 0 ; right < n ; right++)
+            if(nums[right])
+                nums[left++] = nums[right];
+        
+        while(left < n)
+            nums[left++] = 0;
     }
 };
