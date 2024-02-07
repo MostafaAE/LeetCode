036069a90a4dd600d51c111d818d101794b9cf1f
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-02-07 19:07:27 +0200

    Time: 763 ms (34.84%), Space: 192.5 MB (12.12%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,26 +3,27 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
         int n{(int)nums.size()};
-        unordered_map<int, int> hashMap;
         set<vector<int>> unique;
         vector<vector<int>> result;
         
         sort(nums.begin(), nums.end());
         
         for(int i = 0 ; i < n ; i++)
-            hashMap[nums[i]] = i;
-        
-        for(int i = 0 ; i < n ; i++)
         {
-            for(int j = i + 1 ; j < n ; j++)
+            int start{i + 1}, end{n - 1};
+            while(start < end)
             {
-                int target = -1 * (nums[i] + nums[j]);
-                if(hashMap.count(target))
+                if((nums[start] + nums[end]) == (-1*nums[i]))
                 {
-                    int idx = hashMap[target];
-                    if(idx > j && unique.insert({nums[i], nums[j], target}).second)
-                        result.push_back({nums[i], nums[j], target});
+                    if(unique.insert({nums[i], nums[start], nums[end]}).second)
+                        result.push_back({nums[i], nums[start], nums[end]});
+                    start++;
+                    end--;
                 }
+                else if((nums[start] + nums[end]) < (-1 * nums[i]))
+                    start++;
+                else
+                    end--;  
             }
         }
         
