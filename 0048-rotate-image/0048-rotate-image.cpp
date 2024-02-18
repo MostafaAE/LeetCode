9382a697b41a248363c45955962cdb23263528bb
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-02-19 01:53:47 +0200

    Time: 0 ms (100.00%), Space: 8.8 MB (11.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     /* 
     * Approach:
-    * Rotate groups of four cells
+    * Rotate the given matrix two times, first time with respect to the main diagonal, next time rotate the resultant matrix with respect to the middle column.
     * 
     * Complexity:
     * Time Complexity : O(n^2)
@@ -11,16 +11,15 @@ public:
     void rotate(vector<vector<int>>& matrix) 
     {
         int n{(int)matrix.size()};
-        for(int i = 0 ; i < (n+1)/2 ; i++)
-        {
+        
+        // First rotation with respect to main diagonal
+        for(int i = 1 ; i < n ; i++)
+            for(int j = i -1 ; j >= 0 ; j--)
+                swap(matrix[i][j], matrix[j][i]);
+            
+        // Second rotation with respect to middle column
+        for(int i = 0 ; i < n ; i++)
             for(int j = 0 ; j < n/2 ; j++)
-            {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
-                matrix[j][n - 1 -i] = temp;
-            }
-        }
+                swap(matrix[i][j], matrix[i][n-1-j]);
     }
 };
