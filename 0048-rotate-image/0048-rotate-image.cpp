e027d135fc0d8994d849ba2c865022a1a5301f09
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-02-19 02:13:09 +0200

    Time: 4 ms (39.50%), Space: 8.7 MB (11.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     /* 
     * Approach:
-    * Rotate the given matrix two times, first time with respect to the main diagonal, next time rotate the resultant matrix with respect to the middle column.
+    * Rotate the given matrix two times, first time with respect to the main diagonal, next time rotate the resultant matrix with respect to the middle column (transpose then reflect).
     * 
     * Complexity:
     * Time Complexity : O(n^2)
@@ -10,14 +10,24 @@ public:
     */
     void rotate(vector<vector<int>>& matrix) 
     {
+        // First rotation with respect to main diagonal (transpose)
+        transpose(matrix);
+            
+        // Second rotation with respect to middle column (reflect)
+        reflect(matrix);     
+    }
+    
+    void transpose(vector<vector<int>>& matrix)
+    {
         int n{(int)matrix.size()};
-        
-        // First rotation with respect to main diagonal
         for(int i = 1 ; i < n ; i++)
             for(int j = i -1 ; j >= 0 ; j--)
                 swap(matrix[i][j], matrix[j][i]);
-            
-        // Second rotation with respect to middle column
+    }
+    
+    void reflect(vector<vector<int>>& matrix)
+    {
+        int n{(int)matrix.size()};
         for(int i = 0 ; i < n ; i++)
             for(int j = 0 ; j < n/2 ; j++)
                 swap(matrix[i][j], matrix[i][n-1-j]);
