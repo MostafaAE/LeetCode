97ebcae275498fdaaa6600ebcf98ace4e50d5ff2
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-02-20 01:48:47 +0200

    Time: 7 ms (51.43%), Space: 9.5 MB (11.30%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     /* 
+    * Approach: 
+    * Utilize a hash map to make sure that a character always maps to the same character.
+    *
     * Complexity:
-    * Time Complexity : O(L)
-    * Space Complexity : O(1)
+    * Time Complexity : O(n)
+    * Space Complexity : O(n) 
     */
-    bool isIsomorphic(string s, string t) 
-    {
-        vector<int> mapping (256, -1);
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> hashMap1, hashMap2;
         
         for(int i = 0 ; i < (int)s.size() ; i++)
         {
-            if(mapping[s[i]] == -1 && mapping[128+t[i]] == -1)
-            {
-                mapping[s[i]] = t[i];
-                mapping[128+t[i]] = s[i];
-            }
-            else if(mapping[s[i]] != t[i] || mapping[128+t[i]] != s[i])
+            if(hashMap1.contains(s[i]) && hashMap1[s[i]] != t[i] || hashMap2.contains(t[i]) && hashMap2[t[i]] != s[i])
                 return false;
+            hashMap1[s[i]] = t[i];
+            hashMap2[t[i]] = s[i];
         }
+        
         return true;
     }
 };
