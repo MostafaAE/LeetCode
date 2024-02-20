5342c2def034b80ab8b65622adc9910c7f062c5f
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-02-20 15:13:00 +0200

    Time: 27 ms (43.55%), Space: 26.5 MB (52.89%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     /* 
     * Approach:
-    * Iterate on the array two times, while using a stack to find the next greater element for every element 
+    * Use a monotonic decreasing stack for numbers, when we find a greater number than any in the stack, store it as the next greater number (iterate two times).
     * 
     * Complexity:
-    * Time Complexity : O(n)
+    * Time Complexity : O(n) => iterating on elements ~twice only
     * Space Complexity : O(n)
     */
     vector<int> nextGreaterElements(vector<int>& nums) 
@@ -14,17 +14,14 @@ public:
         vector<int> answer(n, -1);
         stack<int> st;
         
-        for(int i = 0 ; i < 2 ; i++)
+        for(int i = 0 ; i < n*2 ; i++)
         {
-            for(int j = 0 ; j < n ; j++)
-            {
-                while(!st.empty() && nums[st.top()] < nums[j])
-                    answer[st.top()] = nums[j], st.pop();
-                
-                if(!i)
-                    st.push(j);
-            }
+            while(!st.empty() && nums[st.top()] < nums[i%n])
+                answer[st.top()] = nums[i%n], st.pop();
+            
+            st.push(i%n);
         }
+        
         return answer;
     }
 };
