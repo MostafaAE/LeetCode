a5457cd59517c0fe258d3c1c33423c88bf5cd99a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-02-28 20:54:55 +0200

    Time: 37 ms (49.18%), Space: 31.1 MB (52.71%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,22 +2,37 @@ class Solution {
 public:
     /* 
     * Approach: 
-    * Sort the array, then return the maximum product of 3 numbers (this could be the 3 largest +ve numbers or the 2 smallest -ve numbers and the largest +ve number).
+    * Utilize a min heap to get the largest 3 numbers, and max heap to get the smallest 2 numbers, then return the maximum product of 3 numbers (this could be the 3 largest +ve numbers or the 2 smallest -ve numbers and the largest +ve number).
     *
     * Complexity:
-    * Time Complexity : O(nlogn)
+    * Time Complexity : O(n)
     * Space Complexity : O(1)
     */
     int maximumProduct(vector<int>& nums) 
     {
         int n{(int)nums.size()};
+        priority_queue<int> maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
         
-        // O(nlogn)
-        sort(nums.begin(), nums.end());
+        for(int i = 0 ; i < n ; i++)
+        {
+            maxHeap.push(nums[i]);
+            minHeap.push(nums[i]);
+            
+            if(minHeap.size() > 3)
+                minHeap.pop();
+            if(maxHeap.size() > 2)
+                maxHeap.pop();
+        }
         
-        int prod1 = nums[n-1] * nums[n-2] * nums[n-3];
-        int prod2 = nums[0] * nums[1] * nums[n-1];
+        // now the min heap have the largest 3 numbers and the max heap have the smallest 2 numbers
+        int m1, m2, m3, s1 ,s2;
+        m1 = minHeap.top(), minHeap.pop();
+        m2 = minHeap.top(), minHeap.pop();
+        m3 = minHeap.top(), minHeap.pop();
+        s1 = maxHeap.top(), maxHeap.pop();
+        s2 = maxHeap.top(), maxHeap.pop();
         
-        return max(prod1, prod2);
+        return max(m1 * m2 * m3 , s1 * s2 * m3);
     }
 };
