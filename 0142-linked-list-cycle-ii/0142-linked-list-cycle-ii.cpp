af4c6efb224872ebcf7f91bcf13302ef8a2512eb
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-03 21:56:01 +0200

    Time: 4 ms (86.69%), Space: 10.1 MB (61.71%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,23 +10,40 @@ class Solution {
 public:
     /*
      * Approach:
-     * Use a hash table to check if a node is repeated.
+     * Use a slow and fast pointer to find the intersection point (where they meet), then move the head and the intersection 1 step until they meet.
+     * Another solution: Use a hash table to check if a node is repeated.
      *
      * Complexity:
      * Time Complexity : O(n)
-     * Space Complexity : O(n)
+     * Space Complexity : O(1)
      */
-    ListNode *detectCycle(ListNode *head) {
+    ListNode *detectCycle(ListNode *head) 
+    {
+        ListNode *intersection = getIntersection(head);
         
-        unordered_set<ListNode *> s;
-        ListNode* cur = head;
-        while(cur && cur->next)
+        if(!intersection)
+            return nullptr;
+        
+        while(head != intersection)
         {
-            // Already exist (cycle)
-            if(!s.insert(cur).second)
-                return cur;
+            head = head->next;
+            intersection = intersection->next;
+        }
             
-            cur = cur->next;
+        return intersection;
+    }
+    
+    ListNode *getIntersection(ListNode *head)
+    {
+        ListNode *slow{head}, *fast{head};
+        
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast)
+                return slow;
         }
         
         return nullptr;
