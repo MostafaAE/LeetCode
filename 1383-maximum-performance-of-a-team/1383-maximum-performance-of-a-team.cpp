98b47dadb69ce90ed7028355ab375896640d479a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-05 01:27:42 +0200

    Time: 94 ms (13.61%), Space: 47.1 MB (12.43%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -0,0 +1,48 @@
+const int MOD = 1e9 + 7;
+class Solution {
+public:
+    /*
+     * Approach:
+     * Sort engineers by efficiency, and use a priority queue to keep track of the highest speed engineers.
+     *
+     * Complexity:
+     * Time Complexity : O(nlogn)
+     * Space Complexity : O(n)
+     */
+    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) 
+    {
+        set<pair<int,int>> efficiencyToIdx;
+        priority_queue<int, vector<int>, greater<int>> speedHeap;
+        long maxPerformanceVal{}, totalSpeed{};
+        
+        // O(nlogn)
+        for(int i = 0 ; i < n ; i++)
+            efficiencyToIdx.insert({efficiency[i], i});
+        
+        auto itr = efficiencyToIdx.rbegin();
+        
+        // O(nlogk)
+        while(itr != efficiencyToIdx.rend())
+        {
+            int idx = itr->second;
+            long curEfficiency = itr->first, curSpeed = speed[idx];
+
+            totalSpeed += curSpeed;
+            
+            speedHeap.push(curSpeed);
+            
+            if(speedHeap.size() > k)
+            {
+                totalSpeed -= speedHeap.top();
+                // O(logk)
+                speedHeap.pop(); 
+            }
+            
+            maxPerformanceVal = max(maxPerformanceVal, totalSpeed * curEfficiency);
+
+            itr++;
+        }
+        
+        return maxPerformanceVal % MOD;
+    }
+};
