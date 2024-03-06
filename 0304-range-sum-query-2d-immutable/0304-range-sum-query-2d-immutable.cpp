2e5a14b842a9e9de1b060744b41870fa25290e86
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-07 00:24:54 +0200

    Time: 300 ms (43.76%), Space: 145.8 MB (75.66%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,27 +1,36 @@
 class NumMatrix {
 private:
-vector<vector<int>> prefix;
+    vector<vector<int>> prefixSum;
 public:
+    
+    // O(n^2)
     NumMatrix(vector<vector<int>>& matrix) 
     {
-        int row{(int)matrix.size()}, col{(int)matrix[0].size()};
-        prefix = vector<vector<int>>(row+1, vector<int>(col+1, 0));
+        int rows{(int)matrix.size()}, cols{(int)matrix[0].size()};
+        prefixSum = matrix;
         
         // accumulate each row
-        for(int i = 1 ; i <= row ; i++)
-            for(int j = 1 ; j <= col ; j++)
-                prefix[i][j] += matrix[i-1][j-1] + prefix[i][j-1];
-
+        for(int r = 0 ; r < rows ; r++)
+            for(int c = 1 ; c < cols ; c++)
+                prefixSum[r][c] += prefixSum[r][c-1];
+        
         // accumulate each column
-        for(int j = 1 ; j <= col ; j++)
-            for(int i = 1 ; i <= row ; i++)
-                prefix[i][j] += prefix[i-1][j];
+        for(int c = 0 ; c < cols ; c++)
+            for(int r = 1 ; r < rows ; r++)
+                prefixSum[r][c] += prefixSum[r-1][c];
+    }
+    
+    int cellSum(int row, int col)
+    {
+        if(row < 0 || col < 0)
+            return 0;
+        return prefixSum[row][col];
     }
     
     // O(1)
     int sumRegion(int row1, int col1, int row2, int col2) 
     {
-        return prefix[row2+1][col2+1] - prefix[row1][col2+1] - prefix[row2+1][col1] + prefix[row1][col1];
+        return cellSum(row2, col2) - cellSum(row1-1, col2) - cellSum(row2, col1-1) + cellSum(row1-1, col1-1);
     }
 };
 
