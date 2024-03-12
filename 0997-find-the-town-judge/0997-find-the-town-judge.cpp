7c2a11f1be40516fd2d579848f73f62d00a8d2e7
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-13 01:26:12 +0200

    Time: 108 ms (91.43%), Space: 64.4 MB (71.53%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,37 +2,26 @@ class Solution {
 public:
     /*
      * Approach:
-     * Build a directed graph, validate rule 1,2, and 3.
+     * Compute the indegree and outdegree of each node, return the node with outdegree = 0 and indegree = n-1.
      *
      * Complexity:
      * Time Complexity : O(V + E)
-     * Space Complexity : O(V + E) where V is the number of nodes, E the number of edges
+     * Space Complexity : O(V) where V is the number of nodes, E the number of edges
      */
     int findJudge(int n, vector<vector<int>>& trust) 
     {
-        vector<unordered_set<int>> adjList(n);
+        vector<int> indegree(n, 0), outdegree(n, 0);
         
         for(auto& v : trust)
-            // add direct edge
-            adjList[v[0]-1].insert(v[1]-1);
+            outdegree[v[0]-1]++, indegree[v[1]-1]++;
         
         // get the node that trusts nobody, with outdegree 0 (rule 1)
-        int judgeCount{}, idx;
+        // and everybody trusts it indegree n-1 (rule 2)
         for(int i = 0 ; i < n ; i++)
             // outdegree = 0
-            if(adjList[i].empty())
-                idx = i, judgeCount++;
+            if(outdegree[i] == 0 && indegree[i] == n-1)
+                return i + 1;
         
-        // violates rule 3
-        if(judgeCount != 1)
-            return -1;
-        
-        // check that everybody trusts this judge (rule 2)
-        for(int i = 0 ; i < n ; i++)
-            // everyone has a direct edge to the judge
-            if(i != idx && !adjList[i].count(idx))
-                return -1;
-        
-        return idx + 1;
+        return -1;
     }
 };
