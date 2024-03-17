f60d2d71e2eebf48bdd3cefd059ebadd8c79a3d9
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-18 01:56:12 +0200

    Time: 0 ms (100.00%), Space: 11.8 MB (7.16%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,23 +11,27 @@
  */
 class Solution {
 public:
-    
-    string parenthesize(TreeNode *node)
-    {
-        if(!node)
-            return "()";
-        
-        string repr = "(" + to_string(node->val);
-        repr += parenthesize(node->left);
-        repr += parenthesize(node->right);
-        repr += ")";
-        
-        return repr;
-    }
+    /* 
+    * Approach:
+    * Apply depth first search, checking if every node in p equals to q.
+    * 
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(n) => auxiliary space for stack
+    */
     bool isSameTree(TreeNode* p, TreeNode* q) 
     {
-        string pRepr = parenthesize(p);
-        string qRepr = parenthesize(q);
-        return (pRepr == qRepr);
+        return sameTree(p, q);
+    }
+    
+    bool sameTree(TreeNode* t1, TreeNode* t2)
+    {
+        if(!t1 && !t2)
+            return true;
+                
+        if(t1 && t2 && t1->val == t2->val)
+            return sameTree(t1->left, t2->left) && sameTree(t1->right, t2->right);
+        
+        return false;    
     }
 };
