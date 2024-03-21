a6a76da5abb0172a3347f4e518a3207c3dc055c2
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-22 01:27:43 +0200

    Time: 81 ms (58.15%), Space: 63.4 MB (93.54%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,41 +2,26 @@ class Solution {
 public:
     /*
      * Approach:
-     * This is a variant of Linked List Cycle II Problem
-     * Use a slow and fast pointer to find the intersection point (where they meet), then move the head and the intersection 1 step until they meet.
+     * Iterate over nums, for each value we find mark it as found by multiplying the value in its index by -1.
+     * 
+     * Another solution: Use a slow and fast pointer to find the intersection point (where they meet), then move the head and the intersection 1 step until they meet. (variant of Linked List Cycle II Problem)
      *
      * Another solution: Use a hash table to check if a number is repeated O(n) memory.
      *
      * Complexity:
      * Time Complexity : O(n)
-     * Space Complexity : O(n)
+     * Space Complexity : O(1)
      */
     int findDuplicate(vector<int>& nums) 
     {
-        int intersection = getIntersection(nums);
-        
-        int head{0};
-        
-        while(head != intersection)
+        for(int i = 0 ; i < (int)nums.size() ; i++)
         {
-            head = nums[head];
-            intersection = nums[intersection];
-        }
-        
-        return intersection;
-    }
-    
-    int getIntersection(vector<int>& nums)
-    {
-        int slow{0}, fast{0};
-        
-        while(true)
-        {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-            
-            if(nums[slow] == nums[fast])
-                return slow;
+            int idx = abs(nums[i]) - 1;
+            // already marked as seen
+            if(nums[idx] < 0)
+                return abs(nums[i]);
+            // mark this value as seen
+            nums[idx] *= -1;
         }
         
         return -1;
