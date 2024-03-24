8937e07a68b681ab3186781fec0c494c4c43b506
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-24 02:36:29 +0200

    Time: 74 ms (77.47%), Space: 63.6 MB (77.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     /*
      * Approach:
-     * Iterate over nums, for each value we find mark it as found by multiplying the value in its index by -1.
-     * 
-     * Another solution: Use a slow and fast pointer to find the intersection point (where they meet), then move the head and the intersection 1 step until they meet. (variant of Linked List Cycle II Problem)
+     * This is a variant of Linked List Cycle II Problem
+     * Use a slow and fast pointer to find the intersection point (where they meet), then move the head and the intersection 1 step until they meet.
      *
      * Another solution: Use a hash table to check if a number is repeated O(n) memory.
+     *
+     * Another solution: Iterate over nums, for each value we find mark it as found by multiplying the value in its index by -1. (modifies the array)
+     *
      *
      * Complexity:
      * Time Complexity : O(n)
@@ -14,14 +16,30 @@ public:
      */
     int findDuplicate(vector<int>& nums) 
     {
-        for(int i = 0 ; i < (int)nums.size() ; i++)
+        int intersection = getIntersection(nums);
+        
+        int head{0};
+        
+        while(head != intersection)
         {
-            int idx = abs(nums[i]) - 1;
-            // already marked as seen
-            if(nums[idx] < 0)
-                return abs(nums[i]);
-            // mark this value as seen
-            nums[idx] *= -1;
+            head = nums[head];
+            intersection = nums[intersection];
+        }
+        
+        return intersection;
+    }
+    
+    int getIntersection(vector<int>& nums)
+    {
+        int slow{0}, fast{0};
+        
+        while(true)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            
+            if(nums[slow] == nums[fast])
+                return slow;
         }
         
         return -1;
