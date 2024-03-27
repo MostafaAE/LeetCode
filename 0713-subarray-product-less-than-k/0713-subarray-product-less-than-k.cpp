cfb1aaf2ecd206a35a78a5fa823ac6a0b1b36889
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-27 03:36:51 +0200

    Time: 52 ms (80.30%), Space: 63.7 MB (13.98%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     /* 
     * Approach:
-    * Sliding window
+    * Utilize a variable-sized sliding window approach to find the number of subarrays with a product less than a given threshold, k. Initially, both left and right pointers are set to the beginning of the array. We continuously shift the right pointer to the right, multiplying the product by the value at the new position. If the product exceeds k, we shrink the window by shifting the left pointer to the right until the product falls below k.
+    *
     * 
     * Complexity:
     * Time Complexity : O(n)
