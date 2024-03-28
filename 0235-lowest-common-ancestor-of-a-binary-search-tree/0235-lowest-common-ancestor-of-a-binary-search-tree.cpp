c485cdd7f8907953b9f3e786a478e1e6e5d08d45
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-28 02:31:00 +0200

    Time: 24 ms (51.93%), Space: 21.8 MB (21.56%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,29 +7,28 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
 class Solution {
 public:
     /* 
+    * Approach:
+    * Depth-first search
+    * 
     * Complexity:
-    * Time Complexity : O(logn)
-    * Space Complexity : O(1)
+    * Time Complexity : O(n)
+    * Space Complexity : O(n)
     */
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        TreeNode * cur = root;
-        while(cur)
-        {
-            if(p->val < cur->val && q->val < cur->val)
-                cur = cur->left;
-        
-            else if(p->val > cur->val && q->val > cur->val)
-                cur = cur->right;
-            
-            else
-                return cur;
-        }
-        
-        return cur; 
+        if(!root || root == p || root == q)
+            return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if(left && right)
+            return root;
+        else if(left)
+            return left;
+        return right;
     }
 };
