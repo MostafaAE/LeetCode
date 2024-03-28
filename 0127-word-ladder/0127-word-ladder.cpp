7df6a948921801765e10a783a955554f7e4012eb
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-03-28 04:30:13 +0200

    Time: 157 ms (46.43%), Space: 20.6 MB (39.61%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -5,30 +5,14 @@ public:
     * Breadth first search.
     * 
     * Complexity:
-    * Time Complexity : O(LN^2)
-    * Space Complexity : O(N) where L is the length of the word and N is the length of wordList
+    * Time Complexity : O(NM^2)
+    * Space Complexity : O(N) where M is the length of the word and N is the length of wordList
     */
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
     {   
-        auto it = find(wordList.begin(), wordList.end(), endWord);
-        if(it == wordList.end())
+        unordered_set<string> hashSet(wordList.begin(), wordList.end());
+        if(!hashSet.count(endWord))
             return 0;
-        
-        wordList.push_back(beginWord);
-        
-        // prepare the adj list
-        unordered_map<string, vector<string>> patternToWords;
-        for(string word : wordList)
-        {
-            for(int i = 0 ; i < (int)word.size() ; i++)
-            {
-                string pattern = word;
-                pattern[i] = '*';
-                if(!patternToWords.count(pattern))
-                    patternToWords[pattern] = vector<string>();
-                patternToWords[pattern].push_back(word);
-            }
-        }
         
         unordered_set<string> visited;
         queue<string> q;
@@ -41,20 +25,19 @@ public:
             int sz = q.size();
             while(sz--)
             {
-                string cur = q.front();
+                string curWord = q.front();
                 q.pop();
                 
-                for(int i = 0 ; i < (int)cur.size() ; i++)
+                for(int i = 0 ; i < (int)curWord.size() ; i++)
                 {
-                    string pattern = cur;
-                    pattern[i] = '*';
-                    for(string word : patternToWords[pattern])
+                    for(char c = 'a' ; c <= 'z' ; c++)
                     {
-                        if(!visited.count(word))
+                        string word = curWord;
+                        word[i] = c;
+                        if(!visited.count(word) && hashSet.count(word))
                         {
                             if(word == endWord)
                                 return level + 1;
-
                             q.push(word);
                             visited.insert(word);
                         }
