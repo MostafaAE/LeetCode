a698f6ccff8110b1f269e509589ed8ed876f52cb
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-04-05 02:31:36 +0200

    Time: 3 ms (48.39%), Space: 8.8 MB (11.17%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    /* 
-    * Complexity:
-    * Time Complexity : O(n)
-    * Space Complexity : O(n)
-    */
+    /*
+     * Approach:
+     * Stack approach.
+     *
+     * Complexity:
+     * Time Complexity : O(n)
+     * Space Complexity : O(n)
+     */
     string makeGood(string s) 
     {
+        string res{};
         stack<char> st;
-        string result{};
         
-        for(int i = 0 ; i < (int)s.size() ; i++)
+        for(char c : s)
         {
-            if(!st.empty() && abs(st.top() - s[i]) == 32)
+            if(!st.empty() && c != st.top() && tolower(c) == tolower(st.top()))
                 st.pop();
             else
-                st.push(s[i]);
+                st.push(c);
         }
         
         while(!st.empty())
-            result = st.top() + result, st.pop();
+            res = st.top() + res, st.pop();
         
-        return result;
-        
+        return res;
     }
 };
