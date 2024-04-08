9f5ca5aa79ff0b5f4b62668a094c93a89850d0f1
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-04-08 23:35:55 +0200

    Time: 3 ms (47.80%), Space: 7.3 MB (72.97%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,49 +1,33 @@
 class Solution {
-private:
-    static const int MAX = 100 + 1;
-    int memory[MAX][MAX];
-    string str;
 public:
-    /*
-     * Approach:
-     * Dynamic Programming Memoization
-     * 
-     * Complexity:
-     * Time Complexity : O(N^2)
-     * Space Complexity : O(N^2)
-     */
+    /* 
+    * Approach:
+    * Two pointers approach.
+    * 
+    * Complexity:
+    * Time Complexity : O(n)
+    * Space Complexity : O(1)
+    */
     bool checkValidString(string s) 
     {
-        memset(memory, -1, sizeof(memory));
-        str = s;
+        int openBrackets{}, closedBrackets{}, n{(int)s.size()};
         
-        return check(0, 0);
-    }
-    
-    bool check(int idx, int openCount)
-    {
-        if(idx >= (int)str.size())
-            return openCount == 0;
+        for(int idx = 0 ; idx < n ; idx++)
+        {
+            if(s[idx] == '(' || s[idx] == '*')
+                ++openBrackets;
+            else
+                --openBrackets;
+            
+            if(s[n-idx-1] ==  ')' || s[n-idx-1] == '*')
+                ++closedBrackets;
+            else
+                --closedBrackets;
+            
+            if(openBrackets < 0 || closedBrackets < 0)
+                return false;
+        }
         
-        if(openCount < 0)
-            return false;
-        
-        int &ret = memory[idx][openCount];
-        if(ret != -1)
-            return ret;
-        
-        if(str[idx] == '(')
-            return ret = check(idx + 1, openCount + 1);
-        else if(str[idx] == ')')
-            return ret = check(idx + 1, openCount - 1);
-        
-        // convert '*' to '('
-        int choice1 = check(idx + 1, openCount + 1);
-        // convert '*' to ''
-        int choice2 = check(idx + 1, openCount);
-        // convert '*' to ')'
-        int choice3 = check(idx + 1, openCount - 1);
-        
-        return ret = choice1 || choice2 || choice3;
+        return true;
     }
 };
