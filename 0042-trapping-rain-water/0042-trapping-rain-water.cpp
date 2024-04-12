64d446c50846b925411cb1e79cafa2fa82abb1ac
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-04-12 03:49:32 +0200

    Time: 13 ms (35.99%), Space: 22 MB (99.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,30 +2,33 @@ class Solution {
 public:
     /* 
     * Approach: 
-    * Two pointers technique
+    * Two pointers technique.
+    * We can also solve it using two arrays (left prefix max, right suffix max), so we can get the max left and max right for each index but will be O(n) memory.
     *
     * Complexity:
     * Time Complexity : O(n)
     * Space Complexity : O(1) 
     */
     int trap(vector<int>& height) {
-        int left{}, right{(int) height.size() - 1}, curHeight{}, water{};
+        int left{}, right{(int)height.size() - 1}, res{};
+        int leftMax{height[left]}, rightMax{height[right]};
         
         while(left < right)
         {
-            if(height[left] < curHeight)
-                water += curHeight - height[left];
-            if(height[right] < curHeight)
-                water += curHeight - height[right];
-            
-            curHeight = max(curHeight, min(height[left], height[right]));
-            
-            if(height[left] < height[right])
-                left++;
+            if(leftMax < rightMax)
+            {
+                ++left;
+                leftMax = max(leftMax, height[left]);
+                res += leftMax - height[left];
+            }
             else
-                right--;
+            {
+                --right;
+                rightMax = max(rightMax, height[right]);
+                res += rightMax - height[right];
+            }
         }
         
-        return water;
+        return res;
     }
 };
