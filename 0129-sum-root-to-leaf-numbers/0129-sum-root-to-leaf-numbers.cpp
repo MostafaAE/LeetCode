16735dd369a6588546917c163a70acd112f4665b
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-04-15 07:47:11 +0200

    Time: 0 ms (100.00%), Space: 10.7 MB (96.99%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     /* 
     * Approach: 
-    * traverse the tree in preorder traversal (dfs) passing the path number from the root
+    * Traverse the tree in preorder traversal (dfs) passing the path number from the root
     * to the next child till we reach a leaf node then sum the root-to-leaf numbers while going
     * back to the root again
     *
@@ -22,18 +22,17 @@ public:
     * Space Complexity : O(h) => auxiliary space for stack
     * h could be (n) or (logn) depending on the shape of the tree
     */
-    int sumNumbers(TreeNode* root, int pathNum = 0) 
+    int sumNumbers(TreeNode* root, int pathSum = 0) 
     {
         if(!root)
             return 0;
         
-        pathNum = pathNum * 10 + root->val;
+        pathSum = pathSum * 10 + root->val;
         
         // leaf node
         if(!root->left && !root->right)
-            return pathNum;
+            return pathSum;
         
-        return sumNumbers(root->left, pathNum) + sumNumbers(root->right, pathNum);
-        
+        return sumNumbers(root->left, pathSum) + sumNumbers(root->right, pathSum);
     }
 };
