9661f564131fdb14ef8dc776a0444995ff76052e
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-04-16 07:46:37 +0200

    Time: 7 ms (48.29%), Space: 12.8 MB (89.99%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -18,35 +18,47 @@ class Solution {
 public:
     /* 
     * Approach:
-    * Iterate over the linkedlist mapping each old node to the new copy node, then iterate again to copy the pointers
-    * 
+    * - Tweak the original linked list. interweave the nodes of the old and copied list. 
+    * For example: Old List: A --> B --> C --> D. InterWeaved List: A --> A' --> B --> B' --> C --> C' --> D --> D'
+    * Then connect the random pointers of the new list to the new nodes and finally split the two lists.
+    *
+    * - Another approach: Utilize a hashmap and iterate over the linkedlist mapping each old node to the new copy node, then iterate again to copy the pointers. O(n) space
+    *
     * Complexity:
     * Time Complexity : O(n)
-    * Space Complexity : O(n)
+    * Space Complexity : O(1) => ignoring the new list space
     */
     Node* copyRandomList(Node* head) 
     {
-        unordered_map<Node*, Node*> oldToCopy{{nullptr, nullptr}};
+        if(!head)
+            return head;
         
-        // map each old node to the new copy node
-        Node* cur = head;
-        while(cur)
+        // tweak the original linked list.
+        // interweave the nodes of the old and copied list.
+        // For example: Old List: A --> B --> C --> D 
+        // InterWeaved List: A --> A' --> B --> B' --> C --> C' --> D --> D'
+        for(Node* cur = head ; cur ; cur = cur->next->next)
         {
-            Node* copy = new Node(cur->val);
-            oldToCopy.insert({cur, copy});
-            cur = cur->next;
+            Node* newNode = new Node(cur->val);
+            newNode->next = cur->next;
+            cur->next = newNode;
         }
         
-        // deep copy the pointers
-        cur = head;
-        while(cur)
+        // connect the random pointers to the new nodes
+        for(Node* cur = head ; cur ; cur = cur->next->next)
+            if(cur->random)
+                cur->next->random = cur->random->next;
+        
+        // split the two linked lists
+        Node* newHead = head->next;
+        for(Node *cur = head ; cur ; cur = cur->next)
         {
-            Node* copy = oldToCopy[cur];
-            copy->next = oldToCopy[cur->next];
-            copy->random = oldToCopy[cur->random];
-            cur = cur->next;
+            Node* copy = cur->next;
+            cur->next = cur->next->next;
+            if(copy->next)
+                copy->next = copy->next->next;
         }
         
-        return oldToCopy[head];
+        return newHead;
     }
 };
