bbf6b42ebc925e1a74432553fad96e814111238f
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-04-25 15:22:04 +0200

    Time: 265 ms (44.12%), Space: 30.5 MB (44.12%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    static const int MAX = 100000 + 1;
+    int memory[MAX][27];
+    string str;
+    int diff;
+public:
+    /*
+     * Approach:
+     * Dynamic Programming Memoization (pick or leave pattern)
+     * Longest Increasing Subsequence (LIS) Variant
+     *
+     * Complexity:
+     * Time Complexity : O(N)
+     * Space Complexity : O(N) where N be the length of s.
+     */
+    int longestIdealString(string s, int k) 
+    {
+        memset(memory, -1, sizeof(memory));
+        str = s;
+        diff = k;
+        return LIS(0, 26);
+    }
+    
+    int LIS(int idx, int lastChar)
+    {
+        if(idx >= str.size())
+            return 0;
+        
+        int &ret = memory[idx][lastChar];
+        if(ret != -1)
+            return ret;
+        
+        int pick{};
+        if(lastChar == 26 || abs(str[idx] - (lastChar + 'a')) <= diff)
+            pick = 1 + LIS(idx + 1, str[idx] - 'a');
+        
+        int leave = LIS(idx + 1, lastChar);
+        
+        return ret = max(pick, leave);
+    }
+};
