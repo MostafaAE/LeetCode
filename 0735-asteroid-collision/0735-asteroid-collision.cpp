944f8d6ced3ee9fa69b754f908139734cdb656b1
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-04-28 17:31:18 +0300

    Time: 13 ms (29.44%), Space: 20.7 MB (29.10%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
-  vector<int> asteroidCollision(vector<int> &asteroids)
-{
-    stack<int> st;
-    vector<int> result;
-    for (int ast : asteroids)
+    /*
+     * Approach:
+     * Simulate asteroid collisions using a stack.
+     * Iterate through the asteroids, and for each asteroid:
+     *   - If it's a negative asteroid and the top of the stack is positive, check for collision:
+     *     - If the top of the stack is larger or equal, destroy the current asteroid.
+     *     - If the current asteroid is larger, destroy the top of the stack and continue checking.
+     *   - If it's a positive asteroid or there are no collisions, push it onto the stack.
+     *
+     * Complexity:
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+    vector<int> asteroidCollision(vector<int>& asteroids) 
     {
-        bool is_exploded = false;
-        while (!st.empty() && ast < 0 && st.top() > 0)
+        stack<int> st;
+
+        for(int ast : asteroids)
         {
-            if (st.top() < -ast)
+            while(!st.empty() && ast < 0 && st.top() > 0)
             {
-                st.pop();
-                continue;
+                if(st.top() >= -ast)
+                {
+                    if(st.top() == -ast)
+                        st.pop();
+                    ast = 0;
+                }
+                else
+                    st.pop();
             }
-            else if (st.top() == -ast)
-                st.pop();
-
-            is_exploded = true;
-            break;
+            if(ast != 0)
+                st.push(ast);
         }
-        if (!is_exploded)
-            st.push(ast);
-    }
 
-    while (!st.empty())
-        result.push_back(st.top()), st.pop();
-    reverse(result.begin(), result.end());
-    return result;
-}
+        vector<int> result(st.size());
+        for(int i = st.size()-1 ; i >= 0 ; --i)
+            result[i] = st.top(), st.pop();
+            
+        return result;
+    }
 };
