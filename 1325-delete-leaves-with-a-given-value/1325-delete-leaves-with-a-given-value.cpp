bd6fc04d1a14223b8df12ce61859467ac535d643
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-05-17 10:34:36 +0300

    Time: 11 ms (64.37%), Space: 21.3 MB (47.43%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -11,14 +11,24 @@
  */
 class Solution {
 public:
-    // Time Complexity = O(n) -> postorder traversal
-    // Space Complexity = O(h) -> stack will contain tree depth
+/*
+    * Approach:
+    * - Use a post-order traversal (left, right, root) to process leaf nodes first.
+    * - Recursively remove leaf nodes with the target value from the left and right subtrees.
+    * - If the current node becomes a leaf node and its value equals the target, return nullptr to remove it.
+    * - Otherwise, return the current node.
+    *
+    * Complexity:
+    * - Time Complexity: O(n), where n is the number of nodes in the binary tree.
+    * - Space Complexity: O(h), where h is the height of the binary tree (auxiliary space for stack `h` could be `(n)` or `(logn)` depending on the shape of the tree).
+    */
     TreeNode* removeLeafNodes(TreeNode* root, int target) 
     {
-        if(root->left)
-            root->left = removeLeafNodes(root->left, target);
-        if(root->right)
-            root->right = removeLeafNodes(root->right, target);
+        if(!root)
+            return nullptr;
+        
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
         
         if(root->val == target && !root->left && !root->right)
             return nullptr;
