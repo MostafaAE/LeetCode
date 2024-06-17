69be4d5f328a46ff5d4aafe0a2ac26b4c861899a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-06-17 04:51:36 +0300

    Time: 3 ms (68.00%), Space: 6.9 MB (91.95%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
-    /* 
+    /*
     * Approach:
-    * iterate on each number started from 0 to the squre root of c to check if there is a valid two integer
-    * 
+    * - Use the two-pointer technique to efficiently find if there are two numbers whose squares sum to c.
+    * - Initialize two pointers: left at 0 and right at sqrt(c).
+    * - Iterate with the while loop:
+    *   - Calculate the sum of the squares of the values pointed by left and right.
+    *   - If the sum equals c, return true.
+    *   - If the sum is less than c, increment the left pointer to increase the sum.
+    *   - If the sum is greater than c, decrement the right pointer to decrease the sum.
+    * - If no pair is found by the time left exceeds right, return false.
+    *
     * Complexity:
-    * Time Complexity : O(sqrt(c)log(c))
-    * Space Complexity : O(1)
+    * - Time Complexity: O(√c)
+    * - Space Complexity: O(1)
     */
     bool judgeSquareSum(int c) 
     {
-        
-        for(long long a = 0 ; a*a <= c ; a++)
-            if(isValid(c,a))
+        long left = 0;
+        long right = sqrt(c);
+
+        // Use the two-pointer approach to find if there are two numbers such that their squares sum to c.
+        while (left <= right) 
+        {
+            long sum = left * left + right * right;
+
+            if (sum == c) 
                 return true;
-        
+            
+            else if (sum < c) 
+                ++left;
+            
+            else 
+                --right;
+        }
+
         return false;
-    }
-    
-    bool isValid(int c, int a)
-    {
-        double b = sqrt(c-a*a);
-        
-        // true if b is integer
-        return (trunc(b) == b);
-        
     }
 };
