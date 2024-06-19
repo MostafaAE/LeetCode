89eee701bc1aa4e7a6df64908cc5f083b28a40ff
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-06-19 04:33:51 +0300

    Time: 105 ms (83.62%), Space: 69 MB (65.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,54 +1,64 @@
 class Solution {
 public:
-    /* 
+    /*
     * Approach:
-    * binary search the minimum number of days
-    * 
+    * - Use binary search to find the minimum number of days needed to pick m bouquets.
+    * - Define the search range from 0 to the maximum value in bloomDay.
+    * - For each midpoint in the binary search: Check if it is possible to pick m bouquets in mid days. If possible, update the result and search in the left half; otherwise, search in the right half.
+    *
     * Complexity:
-    * Time Complexity : O(nlogm) where m is the maximum element in the array
-    * Space Complexity : O(1)
+    * - Time Complexity: O(n logD), where N is the number of flowers and D is the highest value in the array bloomDay.
+    * - Space Complexity: O(1)
     */
     int minDays(vector<int>& bloomDay, int m, int k) 
     {
+        // If there are not enough flowers to make m bouquets, return -1.
         if(m > (int)bloomDay.size() / k)
             return -1;
         
-        int start = 1, end = *max_element(bloomDay.begin(), bloomDay.end()), answer {end};
+        int end = *max_element(bloomDay.begin(), bloomDay.end());
+        int start{}, result = end;
         
+        // Perform binary search to find the minimum number of days.
         while(start <= end)
         {
-            int mid = start + (end - start)/2;
+            int mid = start + (end - start) / 2;
             
-            if(possibleDay(bloomDay, m, k , mid))
-                end = mid - 1, answer = mid;
+            // Check if it is possible to make m bouquets in mid days.
+            if(isPossibleDay(bloomDay, mid, m, k))
+            {
+                result = mid;
+                end = mid - 1;
+            }
             else
                 start = mid + 1;
         }
         
-        return answer;
+        return result;
     }
     
-    bool possibleDay(vector<int>& bloomDay, int m, int k, int day)
+    // Helper function to check if it is possible to make m bouquets in a given number of days.
+    bool isPossibleDay(vector<int>& bloomDay, int days, int m, int k)
     {
-        int cnt{};
-        for(int i = 0 ; i < (int)bloomDay.size() ; i++)
+        int pickedCount{};
+        
+        for(auto val : bloomDay)
         {
-            if(bloomDay[i] <= day)
-                cnt++;
+            if(val <= days)
+                ++pickedCount;
             else
-                cnt = 0;
+                pickedCount = 0;
             
-            if(cnt == k)
+            if(pickedCount == k)
             {
-                cnt = 0;
                 --m;
-                
-                // return early
+                pickedCount = 0;
+                // Return early if m bouquets are made.
                 if(m == 0)
                     return true;
-                
             }
         }
+        
         return m <= 0;
     }
 };
