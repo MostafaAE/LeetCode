eb0e658b3afcf569eced8ec72a2033e06aa07354
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-07-02 06:16:24 +0300

    Time: 4 ms (59.42%), Space: 13.6 MB (34.02%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    
-    /* Approach: 
-     * use unordered_map to store the values and their count of one array
-     * iterate on ther other array and check if element exist in the hashmap and its count > 0
+    /*
+     * Approach:
+     * - If the size of `nums1` is greater than `nums2`, swap them to minimize space complexity.
+     * - Use a hash map to count occurrences of each element in the smaller array.
+     * - Iterate through the larger array and check if each element is in the hash map.
+     * - If an element from the larger array is in the hash map and the count is greater than 0,
+     *   add it to the result and decrease the count in the hash map.
      *
      * Complexity:
-     * Time Complexity : O(n+m)
-     * Space Complexity : O(min(n,m))
+     * - Time Complexity: O(m + n), where m is the size of nums1 and n is the size of nums2.
+     * - Space Complexity: O(min(m, n)), We use a hash map to store counts of elements from the smaller array.
      */
-    
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
     {
-        // What if nums1's size is small compared to nums2's size? Which algorithm is better?
-        // Swap nums1 and nums2 to make nums1 always smaller for minimum space complexity
+        // Ensure nums1 is always the smaller array for optimal space usage.
         if((int)nums1.size() > (int)nums2.size())
             return intersect(nums2, nums1);
-            
-        unordered_map<int, int> ump;
+        
+        unordered_map<int, int> valToCount;
         vector<int> result;
-        
-        for(int i = 0 ; i < (int)nums1.size() ; i++)
-            ump[nums1[i]]++;
-        
-        for(int i = 0 ; i < (int)nums2.size() ; i++)
-            if(ump.count(nums2[i]) && ump[nums2[i]] > 0)
-                ump[nums2[i]]--,result.push_back(nums2[i]);
-        
+
+        // Count occurrences of each element in the smaller array.
+        for(int i = 0; i < (int)nums1.size(); ++i)
+            ++valToCount[nums1[i]];
+
+        // Find common elements by checking the larger array.
+        for(int i = 0; i < (int)nums2.size(); ++i)
+            if(valToCount.count(nums2[i]) && valToCount[nums2[i]] > 0)
+                --valToCount[nums2[i]], result.push_back(nums2[i]);
+
         return result;
     }
 };
