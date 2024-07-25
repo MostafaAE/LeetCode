ee2164e19fa7c7a3c5a529d83b085ddca66131e2
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-07-25 18:14:19 +0300

    Time: 76 ms (94.89%), Space: 71 MB (65.55%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-
-        priority_queue<int> pq;
-
-        //O(nlog(n))
-        for(int i = 0 ; i < (int)nums.size() ; i++)
-            pq.push(-nums[i]);
-
-        //O(nlog(n))
-        for(int i = 0 ; i < (int)nums.size() ; i++)
-            nums[i] = -pq.top(), pq.pop();
+    /**
+     * Approach:
+     * - Use a counting sort algorithm.
+     * - First, determine the minimum and maximum values in the input array.
+     * - Create a count array to store the frequency of each number, adjusted by the minimum value.
+     * - Fill the count array by iterating over the input array and incrementing the appropriate count.
+     * - Reconstruct the sorted array from the count array.
+     *
+     * Complexity:
+     * - Time Complexity: O(n + m), where n is the number of elements in the input array, m is the max value in the array.
+     * - Space Complexity: O(m), where m is the max value in the array.
+     */
+    vector<int> sortArray(vector<int>& nums) 
+    {
+        int size = nums.size();
+        int maxVal = nums[0], minVal = nums[0];
         
+        // get the minimum and max values
+        for(int i = 1 ; i < size ; i++)
+        {
+            if(nums[i] > maxVal)
+                maxVal = nums[i];
+            if(nums[i] < minVal)
+                minVal = nums[i];
+        }
+        
+        int newMax = maxVal - minVal;
+        
+        vector<int> count(newMax +1);
+        
+        for(int i = 0 ; i < size ; i++)
+            count[nums[i]-minVal]++;
+        
+        int idx{};
+        for (int i = 0; i <= newMax; i++)
+        {
+            for (int j = 0; j < count[i]; j++, idx++)
+                nums[idx] = i+minVal;
+        }
         return nums;
-        
     }
 };
