487f3d80185fd456a9696cb95ba330dc4ab818d4
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-10-08 19:45:44 +0300

    Time: 128 ms (24.01%), Space: 35.9 MB (22.98%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    /**
+     * Approach:
+     * - Use a stack to track unmatched brackets as we traverse the string.
+     * - For each character in the string:
+     *   - If the character is ']' and the top of the stack is '[', we pop the stack, representing a valid pair.
+     *   - Otherwise, push the current character onto the stack.
+     * - After processing, the remaining characters in the stack will consist of unmatched brackets.
+     * - The number of swaps needed to balance the string is derived from half the unmatched brackets divided by 2, since each swap fixes two mismatches.
+     * 
+     * Complexity:
+     * - Time Complexity: O(n), where `n` is the length of the string. Each character is processed once.
+     * - Space Complexity: O(n), in the worst case where all characters are pushed onto the stack.
+     */
+    int minSwaps(string s) 
+    {
+        stack<char> st;
+        
+        for(char c : s)
+        {
+            if(c == ']' && !st.empty() && st.top() == '[')
+                st.pop();
+            else
+            {
+                st.push(c);
+            }
+        }
+        
+        return ((st.size() / 2) + 1) / 2;
+    }
+};
