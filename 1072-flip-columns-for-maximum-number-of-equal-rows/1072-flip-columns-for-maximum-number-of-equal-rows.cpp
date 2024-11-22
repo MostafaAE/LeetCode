66769876757c56997bad5bb6a1b1e6d324bf5a84
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-11-22 18:22:30 +0200

    Time: 164 ms (12.32%), Space: 97.8 MB (5.60%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    /**
+     * Approach:
+     *  1. Iterate through each row in the `matrix`.
+     *  2. For each row, build two strings (`positions[0]` and `positions[1]`):
+     *    - `positions[0]`: Represents the column indices for which the value is 0.
+     *    - `positions[1]`: Represents the column indices for which the value is 1.
+     *  3. Use an unordered map (`colPositionsToCount`) to count the occurrences of each pattern of column indices.
+     *    - Increment the count for both `positions[0]` and `positions[1]` in the map.
+     *  4. Keep track of the maximum count (`result`) found across all rows.
+     *  5. The result is the maximum number of rows that can be made identical by flipping columns.
+     * 
+     * Complexity:
+     * - Time Complexity: O(m * n), where m is the number of rows and n is the number of columns.
+     * - Space Complexity: O(m * n), for storing the column positions in the map.
+     */
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) 
     {
         int m = matrix.size(), n = matrix[0].size(), result{};
