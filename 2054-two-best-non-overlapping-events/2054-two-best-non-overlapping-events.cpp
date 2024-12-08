1dfc83d4fb7442869a0eb00ca9b707dfe48ebf6a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-12-08 09:31:52 +0200

    Time: 56 ms (84.23%), Space: 126.1 MB (75.41%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -2,39 +2,24 @@ class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) 
     {
-        int n = events.size(), result{};
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
         sort(events.begin(), events.end());
-        vector<int> suffixMax(n+1, 0);
-        
-        for(int i = n-1 ; i >= 0 ; --i)
-            suffixMax[i] = max(suffixMax[i + 1], events[i][2]);
-            
-        for(int i = 0 ; i < n ; ++i)
+
+        int maxVal = 0, maxSum = 0;
+
+        for (auto& event : events) 
         {
-            int nextNonOverlappingIdx = binarySearch(events, i);
-            
-            result = max(result, events[i][2] + suffixMax[nextNonOverlappingIdx]);
+            while (pq.size() && pq.top().first < event[0]) 
+            {
+                maxVal = max(maxVal, pq.top().second);
+                pq.pop();
+            }
+
+            maxSum = max(maxSum, maxVal + event[2]);
+            pq.push({event[1], event[2]});
         }
-        
-        return result;
-    }
-    
-    int binarySearch(vector<vector<int>>& events, int idx)
-    {
-        int target = events[idx][1];
-        
-        int start = idx + 1, end = events.size() - 1;
-        
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-            
-            if(events[mid][0] <= target)
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
-        
-        return start;
+
+        return maxSum;
     }
 };
