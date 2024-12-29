1338be9f9179a976ea465c2d9dccd57f2ff9b670
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-12-29 17:00:44 +0200

    Time: 0 ms (100%), Space: 7.8 MB (42.18%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    // base (n-2) for any number will always be 12 so the result will always be false
+    // example: 9 in base 7 will be 12, 100 in base 98 will be 12, 10^5 in base 99998 wil be 12
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
