2f28513078a2f25dfa30663d967bb6f6af544265
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2024-12-31 22:49:19 +0200

    Time: 2 ms (66.94%), Space: 12 MB (64.29%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,33 +3,22 @@ public:
     vector<int> minOperations(string boxes) 
     {
         int n = boxes.size(), curState{};
-        vector<int> prefix(n+1, 0), result(n, 0);
+        vector<int> result(n, 0);
 
-        prefix[0] = getBoxValue(boxes[0]);
-        for(int i = 1 ; i < n ; ++i)
+        int ballsToLeft{}, movesToLeft{};
+        int ballsToRight{}, movesToRight{};
+
+        for(int i = 0, j = n-1 ; i < n ; ++i, --j)
         {
-            prefix[i] += prefix[i-1] + getBoxValue(boxes[i]);
-            curState += i * getBoxValue(boxes[i]);
-        }
+            result[i] += movesToLeft;
+            ballsToLeft += boxes[i] - '0';
+            movesToLeft += ballsToLeft;
 
-        prefix[n] = prefix[n-1];
-        result[0] = curState;
-        for(int i = 1 ; i < n ; ++i)
-        {
-            int boxValue = getBoxValue(boxes[i]);
-            int leftDiff = prefix[i] - boxValue; // Get all values to the left
-            int rightDiff = prefix[n] - prefix[i]; // Get all values to the right
-
-            // We got closer to the values to right, and got farther to the values to the left
-            curState = curState - rightDiff + leftDiff - boxValue;
-            result[i] = curState;
+            result[j] += movesToRight;
+            ballsToRight += boxes[j] - '0';
+            movesToRight += ballsToRight;
         }
 
         return result;
-    }
-
-    int getBoxValue(char c)
-    {
-        return c - '0';
     }
 };
