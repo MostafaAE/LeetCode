2e4d6745305b7d2dc3f7f796c3b89be102eb0c3a
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-01-05 06:50:19 +0200

    Time: 0 ms (100%), Space: 72.4 MB (38.46%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.cpp
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -3,18 +3,19 @@ public:
     string shiftingLetters(string s, vector<int>& shifts) 
     {
         int n = s.size();
-        vector<int> diffArray(n, 0);
 
-        diffArray[n-1] = shifts[n-1];
-        for(int i = n-2 ; i >= 0 ; --i)
-            diffArray[i] = (shifts[i] + diffArray[i+1]) % 26;
+        int sum {};
+        for(int i = 0 ; i < n ; ++i)
+            sum = (sum + shifts[i]) % 26;
         
         string shifted(n, ' ');
         for(int i = 0 ; i < n ; ++i)
         {
             int charIdx = s[i] - 'a';
-            int shiftedCharIdx = (charIdx + diffArray[i]) % 26;
+            int shiftedCharIdx = (charIdx + sum) % 26;
             shifted[i] = 'a' + shiftedCharIdx;
+        
+            sum = ((sum - shifts[i]) % 26 + 26) % 26;
         }
 
         return shifted;
