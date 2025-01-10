5cf3034f3fe8f173eb16e0f641e81c8c61c22712
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-01-10 05:12:33 +0200

    Time: 19 ms (5.09%), Space: 64.6 MB (5%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,24 +1,57 @@
+class Trie
+{
+public:
+    static const int MAX_CHAR = 26;
+    Trie *child[MAX_CHAR];
+    bool isLeaf{};
+    int count{}; // Number of words passing through this node
+
+    Trie() 
+    {
+        memset(child, 0, sizeof(child));
+        isLeaf = false;
+        count = 0;
+    }
+
+    void insert(string s)
+    {
+        Trie* cur = this;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            int idx = s[i] - 'a';
+            if (cur->child[idx] == nullptr)
+                cur->child[idx] = new Trie();
+
+            cur = cur->child[idx];
+            ++cur->count;
+        }
+        cur->isLeaf = true;
+    }
+
+    int findPrefixCount(string s)
+    {
+        Trie* cur = this;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            int idx = s[i] - 'a';
+            if (cur->child[idx] == nullptr)
+                return 0; // Prefix does not exist
+
+            cur = cur->child[idx];
+        }
+        return cur->count;
+    }
+};
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) 
     {
-        int count{};
-        
-        for(string& word : words)
-            count += isPrefix(pref, word);
-        
-        return count;
-    }
+        Trie trie;
 
-    bool isPrefix(string& pref, string& word)
-    {
-        if(pref > word)
-            return false;
+        for (string word : words)
+            trie.insert(word);
 
-        for(int i = 0 ; i < pref.size() ; ++i)
-            if(pref[i] != word[i])
-                return false;
-
-        return true;
+        return trie.findPrefixCount(pref);
     }
 };
