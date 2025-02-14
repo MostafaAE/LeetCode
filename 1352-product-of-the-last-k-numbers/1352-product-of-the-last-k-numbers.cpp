0506ceccb1c697d077902bed6ddd09f676b4ea44
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-02-14 15:17:51 +0200

    Time: 36 ms (34.56%), Space: 77.8 MB (93.67%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,36 @@
+class ProductOfNumbers {
+private:
+    vector<int> prefix = {1};
+    int size = 0;
+public:
+    ProductOfNumbers() { }
+    
+    void add(int num) 
+    {
+        if(num == 0)
+        {
+            prefix = {1};
+            size = 0;
+        }
+        else
+        {
+            prefix.push_back(num * prefix.back());
+            ++size;
+        }
+    }
+    
+    int getProduct(int k) 
+    {
+        if(k > size)
+            return 0;
+
+        return prefix[size] / prefix[size - k];
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
