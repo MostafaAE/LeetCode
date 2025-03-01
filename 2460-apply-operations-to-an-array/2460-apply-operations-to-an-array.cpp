ecbdc9d7fb9c6f6dcc9d04b6d4ce7a9adaba2fda
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-03-01 02:34:49 +0200

    Time: 0 ms (100%), Space: 13.2 MB (62.06%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -3,23 +3,26 @@ public:
     vector<int> applyOperations(vector<int>& nums) 
     {
         int n = nums.size(), idx{};
-
-        nums.push_back(0);
-        vector<int> result(n, 0);
         
-        for(int i = 0 ; i < n ; ++i)
+        for(int i = 0 ; i < n - 1 ; ++i)
         {
-            if(nums[i] != 0 && nums[i] != nums[i+1])
+            if(nums[i] != 0 && nums[i] == nums[i + 1])
             {
-                result[idx++] = nums[i];
-            }
-            else if(nums[i] != 0 && nums[i] == nums[i + 1])
-            {
-                result[idx++] = nums[i] * 2;
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
                 ++i;
             }
         }
+
+        for(int i = 0 ; i < n ; ++i)
+        {
+            if(nums[i] != 0)
+            {
+                swap(nums[idx], nums[i]);
+                ++idx;
+            }
+        }
         
-        return result;
+        return nums;
     }
 };
