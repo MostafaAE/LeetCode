c2656f8fc8f60795fb30ee775cc405877f5f768f
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-04-27 06:08:36 +0300

    Time: 35 ms (77.2%), Space: 10.3 MB (100%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,22 +1,8 @@
-/**
- * Approach:
- * - Use Union-Find (Disjoint Set Union) to group similar strings.
- * - Two strings are similar if we can make them equivalent by swapping at most two letters.
- * - Compare every pair of strings. If they are similar, union them.
- * - After all unions, the number of groups is the number of forests remaining.
- * 
- * Time Complexity: O(n^2 * m), where
- *   - n = number of strings
- *   - m = length of each string (cost to compare two strings)
- * Space Complexity: O(n + m)
- */
-
 class UnionFind
 {
 private:
     vector<int> rank, parent;
-    int forests;
-
+    
     void link(int x, int y)
     {
         if (rank[x] > rank[y])
@@ -28,38 +14,33 @@ private:
     }
 
 public:
+    int forests;
     UnionFind(int n)
     {
-        rank = vector<int>(n);
-        parent = vector<int>(n);
+        rank = vector<int>(n), parent = vector<int>(n);
         forests = n; // Number of the current forests
 
         for (int i = 0; i < n; ++i)
-        {
-            parent[i] = i;
-            rank[i] = 1;
-        }
+            parent[i] = i, rank[i] = 1;
     }
 
     int find_set(int x)
     {
         if (x == parent[x])
             return x;
-
+        // This is the path compression
+        // the top parent is returned and we re-link
         return parent[x] = find_set(parent[x]);
     }
 
     bool union_sets(int x, int y)
     {
-        x = find_set(x);
-        y = find_set(y);
-
+        x = find_set(x), y = find_set(y);
         if (x != y)
-        {
+        { // Different components
             link(x, y);
-            forests--;
+            forests--; // 2 merged into 1
         }
-        
         return x != y;
     }
 
@@ -69,40 +50,37 @@ public:
     }
 };
 
+
 class Solution {
-private:
+public:
     bool isSimilar(string &str1, string &str2)
     {
-        int n = str1.size();
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++)
+        int n{(int)str1.size()};
+        int cnt{};
+        for(int i = 0 ; i < n ; i++)
         {
-            if (str1[i] != str2[i])
+            if(str1[i] != str2[i])
                 cnt++;
             
-            if (cnt > 2)
+            if(cnt > 2)
                 return false;
         }
-
-        return true;
+        
+        return true;;
     }
-
-public:
+    
     int numSimilarGroups(vector<string>& strs) 
     {
-        int n = strs.size();
+        int n{(int)strs.size()};
+       
         UnionFind uf(n);
         
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = i - 1; j >= 0; j--)
-            {
-                if (isSimilar(strs[i], strs[j]))
+        for(int i = 1 ; i < n ; i++)
+            for(int j = i - 1 ; j >= 0 ; j--)
+                if(isSimilar(strs[i], strs[j]))
                     uf.union_sets(i, j);
-            }
-        }
+            
+        return uf.forests;
         
-        return uf.getForests();
     }
 };
