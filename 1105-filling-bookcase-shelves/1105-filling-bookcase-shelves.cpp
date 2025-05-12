1bc4590ad615604901d630e93c3470b103e9e9c8
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-05-12 22:46:31 +0300

    Time: 3 ms (34.19%), Space: 8.5 MB (100%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,46 +1,57 @@
 const int MAX = 1000 + 1;
-int memory[MAX];
+int memory[MAX], shelfWidth;
+vector<vector<int>> books;
 
 class Solution {
 public:
     /*
      * Approach:
-     * Dynamic Programming Tabulation
+     * Dynamic Programming Memoization
      * 
      * Direct Consecutive range pattern
      * We want to find a good blocks split
+     *
+     * dp(idx)
+     *  Try all consecutive ranges as long as total width <= shelfWidth
+     *      For each choice [idx, k] subarray, recurse on the remaining dp(k+1)
+     *		The choice cost is the max height in the subarray [idx, k]
      *
      * Complexity:
      * Time Complexity : O(N^2)
      * Space Complexity : O(N) where N is the max index
      */
-    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) 
-    {   
-        for(int i = 0 ; i < (int)books.size() ; i++)
-        {
-            memory[i] = INT_MAX;
-            int booksHeight{}, booksThickness{};
-            for(int split = i ; split >= 0 ; split--)
-            {
-                booksThickness += books[split][0];
-
-                if(booksThickness > shelfWidth)
-                    break;
-
-                booksHeight = max(booksHeight, books[split][1]);
-
-                memory[i] = min(memory[i], booksHeight + answer(split - 1));
-            }
-        }
-            
-       return memory[(int)books.size() - 1];
+    int minHeightShelves(vector<vector<int>>& books_, int shelfWidth_) 
+    {
+        books = books_;
+        shelfWidth = shelfWidth_;
+        memset(memory, -1, sizeof(memory));
+        
+        return bestSplit(0);
     }
     
-    int answer(int index)
+    int bestSplit(int index)
     {
-        if(index <= -1)
+        if(index >= (int)books.size())
             return 0;
         
-        return memory[index];
+        int &ret = memory[index];
+        if(ret != -1)
+            return ret;
+        
+        ret = INT_MAX;
+        int booksHeight{}, booksThickness{};
+        for(int split = index ; split < (int)books.size() ; split++)
+        {
+            booksThickness += books[split][0];
+            
+            if(booksThickness > shelfWidth)
+                break;
+            
+            booksHeight = max(booksHeight, books[split][1]);
+            
+            ret = min(ret, booksHeight + bestSplit(split+1));
+        }
+        
+        return ret;
     }
 };
