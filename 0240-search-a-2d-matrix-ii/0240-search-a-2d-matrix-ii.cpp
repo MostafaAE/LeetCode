49223cd68a8ede5f7ff91c032addd23794f815a8
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-06-25 21:24:02 +0300

    Time: 51 ms (68.32%), Space: 18.5 MB (92.22%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,28 +1,36 @@
+/*
+ * Approach:
+ * Start from the top-right corner of the matrix.
+ * - If the current value is equal to the target, return true.
+ * - If the current value is greater than the target, move left.
+ * - If the current value is less than the target, move down.
+ * 
+ * This works because:
+ * - Each row is sorted in increasing order (left to right)
+ * - Each column is sorted in increasing order (top to bottom)
+ *
+ * Time Complexity : O(m + n), where m is the number of rows and n is the number of columns
+ * Space Complexity: O(1), no extra space used
+ */
+
 class Solution {
 public:
-    /* 
-    * Complexity:
-    * Time Complexity : O(m+n)
-    * Space Complexity : O(1)
-    */
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
-        int m{(int)matrix.size()}, n{(int)matrix[0].size()};
+        int m = (int)matrix.size(), n = (int)matrix[0].size();
         
-        int row{}, col{n-1};
-        
-        while(row < m && col > -1)
+        int row = 0, col = n - 1;
+
+        while (row < m && col >= 0)
         {
-            if(target == matrix[row][col])
+            if (matrix[row][col] == target)
                 return true;
-            
-            if(target < matrix[row][col])
-                col--;
+            else if (target < matrix[row][col])
+                --col;
             else
-                row++;
+                ++row;
         }
-        
+
         return false;
-        
     }
 };
