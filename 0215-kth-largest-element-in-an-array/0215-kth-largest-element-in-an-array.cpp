d46ad29a718d4d43b3ec9cbdc3e5f353922c6ec6
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-06-26 17:56:14 +0300

    Time: 44 ms (15.89%), Space: 61.5 MB (60.32%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,24 +1,34 @@
+/*
+ * Approach:
+ * To find the k-th largest element, we use a min-heap of size `k`.
+ * 
+ * - Traverse the array and maintain a min-heap with at most `k` elements.
+ * - For each number, insert it into the heap.
+ * - If the heap size exceeds `k`, remove the smallest element (heap top).
+ * - After processing all numbers, the top of the min-heap will be the k-th largest element.
+ *
+ * Time Complexity  : O(n log k), where n is the number of elements in the array.
+ *                    Each insertion/deletion in the heap takes O(log k).
+ * Space Complexity : O(k), for storing at most `k` elements in the heap.
+ */
+
 class Solution {
 public:
-    /* 
-    * Approach:
-    * Utilize a priority queue as a min heap and use it to keep track of the k largest elements, the answer will be the top of it.
-    * Complexity:
-    * Time Complexity : O(nlogk)
-    * Space Complexity : O(k)
-    */
     int findKthLargest(vector<int>& nums, int k) 
     {
-        priority_queue<int, vector<int>, greater<int>> mnHeap;
-        
-        for(int val : nums)
+        // Min-heap to store the k largest elements
+        priority_queue<int, vector<int>, greater<>> minHeap;
+
+        for (int num : nums)
         {
-            if(mnHeap.size() >= k && val >= mnHeap.top())
-                mnHeap.pop();
-            
-            if(mnHeap.size() < k)
-                mnHeap.push(val);
-        }    
-        return mnHeap.top();
+            minHeap.push(num);
+
+            // Remove the smallest if we have more than k elements
+            if (minHeap.size() > k)
+                minHeap.pop();
+        }
+
+        // The top of the heap is the k-th largest element
+        return minHeap.top();
     }
 };
