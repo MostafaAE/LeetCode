96f5aa34286b0acac0876e51f845eef62265a2ca
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-06-27 15:12:53 +0300

    Time: 3 ms (35.18%), Space: 63 MB (48.12%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,28 +1,38 @@
+/*
+ * Approach:
+ * - We use the two-pointer technique to solve this problem optimally.
+ * - Start with two pointers, one at the beginning and one at the end of the height array.
+ * - The area formed between the lines is determined by the shorter line and the distance between them.
+ * - Move the pointer pointing to the shorter line inward, since moving the taller line would not help (as the height is limited by the shorter one).
+ * - Repeat this process until the two pointers meet, tracking the maximum area seen so far.
+ *
+ * Time Complexity  : O(n), where n is the number of elements in the input vector.
+ * Space Complexity : O(1), constant space is used.
+ */
+
 class Solution {
 public:
-    /* 
-    * Approach: 
-    * Two pointers approach, set one pointer to the left and one to the right of the array, move the pointer that points to the lower line.
-    *
-    * Complexity:
-    * Time Complexity : O(n)
-    * Space Complexity : O(1)
-    */
     int maxArea(vector<int>& height) 
     {
-        int left{}, right{(int)height.size()-1};
-        int maxWater{};
-        while(left < right)
+        int left = 0;
+        int right = (int)height.size() - 1;
+        int maxWater = 0;
+
+        // Two-pointer approach: move inward from both ends
+        while (left < right)
         {
-            int curWater = (right - left)*min(height[left], height[right]);
+            // Calculate the current area using the shorter of the two heights
+            int curWater = (right - left) * min(height[left], height[right]);
+
             maxWater = max(maxWater, curWater);
-            
-            if(height[left] < height[right])
-                left++;
+
+            // Move the pointer pointing to the shorter height
+            if (height[left] < height[right])
+                ++left;
             else
-                right--;
+                --right;
         }
-        
+
         return maxWater;
     }
 };
