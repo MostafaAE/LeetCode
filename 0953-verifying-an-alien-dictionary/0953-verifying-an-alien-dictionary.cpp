f3d0f5c79e8dbb0e1cb7a78cc566cdd2dce19cf7
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-07-05 18:40:57 +0300

    Time: 9 ms (3.72%), Space: 12.4 MB (84.72%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    unordered_map<char, int> charToIdx;
     /* 
     * Approach: 
-    * Use a hashmap to map each alien character to its index, then compare every two consecutive strings to check if they are sorted lexicographically.
+    * Use a hashmap to map each alien character to its index, then compare every two consequetive strings to check if they are sorted lexicographically.
     *
     * Complexity:
     * Time Complexity : O(N*L)
@@ -11,24 +10,28 @@ public:
     */
     bool isAlienSorted(vector<string>& words, string order) 
     {
+        unordered_map<char, int> charToIdx;
+        
         for(int i = 0 ; i < (int)order.size() ; i++)
             charToIdx[order[i]] = i;
         
         for(int i = 1 ; i < (int)words.size() ; i++)
-            if(words[i - 1] != words[i] && !isSmaller(words[i - 1] , words[i]))
+        {
+            int s1 = words[i-1].size(), s2 = words[i].size(), equals{};
+            
+            for(int j = 0 ; j < min(s1, s2) ; j++)
+            {
+                if(charToIdx[words[i-1][j]] < charToIdx[words[i][j]])
+                    break;
+                else if(charToIdx[words[i-1][j]] > charToIdx[words[i][j]])
+                    return false;
+                else
+                    equals++;
+            }
+            if(equals == s2 && s1 > s2)
                 return false;
+        }
         
         return true;
-    }
-    
-    bool isSmaller(string &a, string &b)
-    {
-        int s1 = a.size(), s2 = b.size();
-        
-        for(int i = 0 ; i < min(s1, s2) ; i++)
-            if(a[i] != b[i])
-                return charToIdx[a[i]] < charToIdx[b[i]];
-        
-        return s1 < s2;
     }
 };
