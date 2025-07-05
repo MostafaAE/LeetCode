ddd30f3b281a0a56a4f6380e5969591a227bd5d4
Mostafa Ayman <47731377+MostafaAE@users.noreply.github.com>
2025-07-05 19:00:02 +0300

    Time: 0 ms (100%), Space: 12.6 MB (50.28%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     /* 
     * Approach: 
-    * Use a hashmap to map each alien character to its index, then compare every two consequetive strings to check if they are sorted lexicographically.
+    * Use a hashmap to map each alien character to its index, then compare every two consecutive strings to check if they are sorted lexicographically.
     *
     * Complexity:
     * Time Complexity : O(N*L)
@@ -11,27 +11,30 @@ public:
     bool isAlienSorted(vector<string>& words, string order) 
     {
         unordered_map<char, int> charToIdx;
-        
         for(int i = 0 ; i < (int)order.size() ; i++)
+        {
             charToIdx[order[i]] = i;
+        }
         
         for(int i = 1 ; i < (int)words.size() ; i++)
         {
-            int s1 = words[i-1].size(), s2 = words[i].size(), equals{};
-            
-            for(int j = 0 ; j < min(s1, s2) ; j++)
-            {
-                if(charToIdx[words[i-1][j]] < charToIdx[words[i][j]])
-                    break;
-                else if(charToIdx[words[i-1][j]] > charToIdx[words[i][j]])
-                    return false;
-                else
-                    equals++;
-            }
-            if(equals == s2 && s1 > s2)
+            if(!isSorted(words[i - 1] , words[i], charToIdx))
                 return false;
         }
         
         return true;
+    }
+    
+    bool isSorted(string &a, string &b, unordered_map<char, int>& charToIdx)
+    {
+        int s1 = a.size(), s2 = b.size();
+        
+        for(int i = 0 ; i < min(s1, s2) ; i++)
+        {
+            if(a[i] != b[i])
+                return charToIdx[a[i]] < charToIdx[b[i]];
+        }
+        
+        return s1 <= s2;
     }
 };
