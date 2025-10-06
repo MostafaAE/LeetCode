52a849fa3e6674a8fb38ee3948f2a7cc563d24b0
Mostafa Elsafty <47731377+MostafaAE@users.noreply.github.com>
2025-10-06 19:25:47 +0300

    Time: 2 ms (91.01%), Space: 14.5 MB (37.12%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        int low = grid[0][0], hi = n*n - 1;
+        while (low < hi) 
+        {
+            int mid = low + (hi-low) / 2;
+            if (valid(grid, mid)) 
+               hi = mid;
+            else
+               low = mid+1;
+        }
+
+        return low;
+    }
+private:
+    bool valid(vector<vector<int>>& grid, int waterHeight) 
+    {
+        int n = grid.size();
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        vector<int> dir({-1, 0, 1, 0, -1});
+        return dfs(grid, visited, dir, waterHeight, 0, 0, n);
+    }
+
+    bool dfs(vector<vector<int>>& grid, vector<vector<int>>& visited, vector<int>& dir, int waterHeight, int row, int col, int n) 
+    {
+        visited[row][col] = 1;
+        for (int i = 0; i < 4; ++i) 
+        {
+            int r = row + dir[i], c = col + dir[i+1];
+            if (r >= 0 && r < n && c >= 0 && c < n && visited[r][c] == 0 && grid[r][c] <= waterHeight) 
+            {
+                if (r == n-1 && c == n-1) 
+                    return true;
+                if (dfs(grid, visited, dir, waterHeight, r, c, n)) 
+                    return true;
+            }
+        }
+        return false;
+    }
+};
