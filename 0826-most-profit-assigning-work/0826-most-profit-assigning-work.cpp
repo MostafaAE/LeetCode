cf8b1eeae6a740ba39e3e22ca234ee77789e8c39
Mostafa Elsafty <47731377+MostafaAE@users.noreply.github.com>
2026-07-27 00:37:19 +0300

    Time: 7 ms (85.84%), Space: 41.6 MB (60.99%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
-    /*
-    * Approach:
-    * - Pair each job's difficulty with its profit and sort the jobs by difficulty.
-    * - Sort the workers by their ability.
-    * - Iterate over each worker, and for each worker, find the maximum profit they can achieve based on their ability.
-    * - Use a two-pointer technique to keep track of the most profitable job that each worker can do.
-    * 
-    * Complexity:
-    * - Time Complexity: O(n log n + m log m)
-    *   - Sorting the jobs takes O(n log n).
-    *   - Sorting the workers takes O(m log m).
-    *   - The iteration over the workers and jobs together takes O(m + n).
-    * - Space Complexity: O(n)
-    *   - The extra space used is for storing the job pairs.
-    */
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) 
     {
-        int totalProfit{};
-        vector<pair<int, int>> jobs;
-        
-        // Pair each job's difficulty with its profit
-        for(int i = 0 ; i < (int)profit.size() ; i++)
-            jobs.push_back({difficulty[i], profit[i]});
-        
-        // Sort jobs by difficulty
-        sort(jobs.begin(), jobs.end());
-        
-        // Sort workers by their ability
-        sort(worker.begin(), worker.end());
-        
-        int job{}, maxProfitSoFar{};
-        // Iterate over each worker
-        for(int i = 0 ; i < (int)worker.size() ; i++)
+        int n = difficulty.size(), m = worker.size();
+        vector<pair<int, int>> profitToDifficulty;
+
+        for(int i = 0 ; i <  n ; ++i)
         {
-            // Find the most profitable job the worker can do
-            while(job < jobs.size() && worker[i] >= jobs[job].first)
-            {
-                maxProfitSoFar = max(maxProfitSoFar, jobs[job].second);
-                job++;
-            }
-            
-            // Add the maximum profit the worker can achieve to the total profit
-            totalProfit += maxProfitSoFar;
+            profitToDifficulty.push_back({profit[i], difficulty[i]});
         }
-        
-        return totalProfit;
+
+        sort(worker.begin(), worker.end(), greater<>());
+        sort(profitToDifficulty.begin(), profitToDifficulty.end(), greater<>());
+
+        int maxProfit = 0;
+        for(int i = 0, j = 0; i < m && j < n; ++i)
+        {
+            while(worker[i] < profitToDifficulty[j].second)
+                ++j;
+            
+            if(j < n && worker[i] >= profitToDifficulty[j].second)
+                maxProfit += profitToDifficulty[j].first;
+        }
+
+        return maxProfit;
     }
 };
