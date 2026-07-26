691ab1bbe5a9cff3ee97091ee674473b39864afe
Mostafa Elsafty <47731377+MostafaAE@users.noreply.github.com>
2026-07-26 21:40:13 +0300

    Time: 0 ms (100%), Space: 31.6 MB (10.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     /* 
     * Approach: 
-    * Utilize a min heap to get the largest 3 numbers, and max heap to get the smallest 2 numbers, then return the maximum product of 3 numbers (this could be the 3 largest +ve numbers or the 2 smallest -ve numbers and the largest +ve number).
+    * Utilize a min heap to get the largest 3 numbers, and max heap to get the smallest 2 numbers, then return the maximum 
+    * product of 3 numbers (this could be the 3 largest +ve numbers or the 2 smallest -ve numbers and the largest +ve number).
     *
     * Complexity:
     * Time Complexity : O(n)
@@ -26,13 +27,14 @@ public:
         }
         
         // now the min heap have the largest 3 numbers and the max heap have the smallest 2 numbers
-        int m1, m2, m3, s1 ,s2;
-        m1 = minHeap.top(), minHeap.pop();
-        m2 = minHeap.top(), minHeap.pop();
-        m3 = minHeap.top(), minHeap.pop();
-        s1 = maxHeap.top(), maxHeap.pop();
-        s2 = maxHeap.top(), maxHeap.pop();
+        int result1 = 1, result2 = 1;
+        for(int i = 0 ; i < 2 ; ++i)
+        {
+            result1 *= minHeap.top(), minHeap.pop();
+            result2 *= maxHeap.top(), maxHeap.pop();
+        }
         
-        return max(m1 * m2 * m3 , s1 * s2 * m3);
+        int maxValue = minHeap.top();
+        return max(maxValue * result1, maxValue * result2);
     }
 };
