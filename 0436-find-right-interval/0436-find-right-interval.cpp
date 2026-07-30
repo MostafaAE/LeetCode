b7b3c5223d11295de61b55088553688a1f1e7423
Mostafa Elsafty <47731377+MostafaAE@users.noreply.github.com>
2026-07-30 23:10:55 +0300

    Time: 16 ms (47.76%), Space: 32.4 MB (29.57%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    /* 
-    * Approach:
-    * use a map to map each start with its index, then find the right interval
-    * for each end using map lower_bound
-    * 
-    * Complexity:
-    * Time Complexity : O(nlogn)
-    * Space Complexity : O(n)
-    */
     vector<int> findRightInterval(vector<vector<int>>& intervals) 
     {
-        map<int, int> startToIdx;
-        vector<int> result(intervals.size(), -1);
-        
-        // O(nlogn)
-        for(int i = 0 ; i < (int)intervals.size() ; i++)
-            startToIdx[intervals[i][0]] = i;
-        
-        // O(nlogn)
-        for(int i = 0 ; i < (int)intervals.size() ; i++)
+        int n = intervals.size();
+        map<int, int> startToIndex;
+
+        for(int i = 0 ; i < n ; ++i)
         {
-            auto lb = startToIdx.lower_bound(intervals[i][1]);
-            
-            if(lb != startToIdx.end())
-                result[i] = lb->second;
+            startToIndex[intervals[i][0]] = i;
         }
-        
+
+        vector<int> result(n, -1);
+        for(int i = 0 ; i < n ; ++i)
+        {
+            auto lb = startToIndex.lower_bound(intervals[i][1]);
+            if(lb != startToIndex.end())
+            {
+                result[i] = lb->second;
+            }
+        }
+
         return result;
     }
 };
