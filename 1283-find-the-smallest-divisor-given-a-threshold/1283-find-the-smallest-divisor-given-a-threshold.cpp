70941eb00131f857438319b6c78689d5a317371f
Mostafa Elsafty <47731377+MostafaAE@users.noreply.github.com>
2026-08-01 22:24:38 +0300

    Time: 10 ms (43.28%), Space: 26 MB (98.61%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
+    /* 
+    * Approach:
+    * binary search the smallest possible divisor
+    * 
+    * Complexity:
+    * Time Complexity : O(nlogm) where m is the maximum element in the array
+    * Space Complexity : O(1)
+    */
     int smallestDivisor(vector<int>& nums, int threshold) 
     {
-        int left = 1, right = 1000000, result{};
+        int start = 1, end = *max_element(nums.begin(), nums.end()), answer{end};
 
-        while(left <= right)
+        while(start <= end)
         {
-            int mid = left + (right - left) / 2;
-
-            if(isValid(nums, mid, threshold))
-            {
-                result = mid;
-                right = mid - 1;
-                
-            }
-            else
-                left = mid + 1;
-        }
-
-        return result;
-    }
-
-    bool isValid(vector<int>& nums, int divisor, int threshold)
-    {
-        int divSum{};
-
-        for(int num : nums)
-            divSum += ceil(num / (double)divisor);
+            int mid = start + (end - start)/2;
             
-        return divSum <= threshold;
+            if(possibleDivisor(nums, mid, threshold))
+                end = mid - 1, answer = mid;
+            else
+                start = mid + 1;
+        }
+        
+        return answer;
+    }
+    
+    bool possibleDivisor(vector<int>& nums, int d, int threshold)
+    {
+        long long sum{};
+        
+        for(int elem : nums)
+            sum += (elem + d-1)/d;
+        
+        return sum <= threshold;
     }
 };
